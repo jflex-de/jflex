886b41adccc5d4bb0eb4f41abe1d95cc20cc6dc9
Gerwin Klein <lsf37@doclsf.de>
2003-06-04 22:08:02 +0000

    long name --outdir for -d (in analogy to JFlexTask)
    svn-rev: trunk/jflex@25

--- src/JFlex/Main.java
+++ src/JFlex/Main.java
@@ -150,7 +150,7 @@ public class Main implements ErrorMessages {
 
     for (int i = 0; i < argv.length; i++) {
 
-      if ( argv[i].equals("-d") ) {
+      if ( argv[i].equals("-d") || argv[i].equals("--outdir") ) {
         if ( ++i >= argv.length ) {
           Out.error(NO_DIRECTORY); 
           System.exit(1);
