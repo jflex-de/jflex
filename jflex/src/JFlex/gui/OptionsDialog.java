5508ccba367c7daebd7d168a5e84d91ff66dbcb2
Gerwin Klein <lsf37@doclsf.de>
2003-06-30 15:12:20 +0000

    new options dialog, layout still ugly, skeleton doesn't work yet
    svn-rev: trunk@77

--- jflex/src/JFlex/gui/OptionsDialog.java
+++ jflex/src/JFlex/gui/OptionsDialog.java
@@ -21,6 +21,12 @@
 package JFlex.gui;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import JFlex.Options;
 
@@ -32,18 +38,24 @@ import JFlex.Options;
  */
 public class OptionsDialog extends Dialog {
 
+  private Button skelBrowse;
   private TextField skelDir;
 
   private Button ok;
-  private Button cancel;
   private Button defaults;
 
-  private Checkbox skel;
   private Checkbox dump;
   private Checkbox verbose;
+  private Checkbox jlex;
+  private Checkbox no_minimize; 
+  private Checkbox no_backup; 
+  private Checkbox time;
+  private Checkbox dot;
+
   private Checkbox tableG;
   private Checkbox switchG;
-  private Checkbox packG;       
+  private Checkbox packG; 
+  
 
   /**
    * Create a new options dialog
@@ -52,22 +64,160 @@ public class OptionsDialog extends Dialog {
    */
   public OptionsDialog(Frame owner) {
     super(owner, "Options");
+
+    addWindowListener( new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        close();
+      }
+    });
+    
     setup();
+    pack();
   }
 
   public void setup() {
+    // create components
     ok = new Button("Ok");
-    cancel = new Button("Cancel");
-    
-    skel = new Checkbox("use skeleton file");
-    dump = new Checkbox("dump");
-    verbose = new Checkbox("verbose");
+    defaults = new Button("Defaults");    
+    skelBrowse = new Button(" Browse");
+    skelDir = new TextField();
+    skelDir.setEditable(false);
+    dump = new Checkbox(" dump");
+    verbose = new Checkbox(" verbose");
+
+    jlex = new Checkbox(" JLex compatibility");
+    no_minimize = new Checkbox(" skip minimization");
+    no_backup = new Checkbox(" no backup file");
+    time = new Checkbox(" time statistics");
+    dot = new Checkbox(" dot graph files");
 
     CheckboxGroup codeG = new CheckboxGroup();
-    tableG = new Checkbox("table",Options.gen_method == Options.TABLE, codeG);
-    switchG = new Checkbox("switch",Options.gen_method == Options.SWITCH, codeG);
-    packG = new Checkbox("pack",Options.gen_method == Options.PACK, codeG);
+    tableG = new Checkbox(" table",Options.gen_method == Options.TABLE, codeG);
+    switchG = new Checkbox(" switch",Options.gen_method == Options.SWITCH, codeG);
+    packG = new Checkbox(" pack",Options.gen_method == Options.PACK, codeG);
     
+    // setup interaction
+    ok.addActionListener( new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        close();
+      }
+    } );
+
+    defaults.addActionListener( new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setDefaults();
+      }
+    } );
+
+    tableG.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        setGenMethod();
+      }
+    } );
+
+    dump.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.dump = dump.getState();                    
+      }
+    } );
+
+    jlex.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.jlex = jlex.getState();                    
+      }
+    } );
+
+    no_minimize.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.no_minimize = no_minimize.getState();                    
+      }
+    } );
+    
+    no_backup.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.no_backup = no_backup.getState();                    
+      }
+    } );
+
+    dot.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.dot = dot.getState();                    
+      }
+    } );
+
+    time.addItemListener( new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        Options.time = time.getState();                    
+      }
+    } );
+
+    // setup layout
+    GridPanel panel = new GridPanel(4,7,10,10);
+    panel.setInsets( new Insets(10,5,5,10) );
+    
+    panel.add(3,0,ok);
+    panel.add(3,1,defaults);
+     
+    panel.add(0,0,1,1,new Label(" use skeleton file"));
+    panel.add(0,1,2,1,skelDir);
+    panel.add(2,1,1,1,Handles.TOP, skelBrowse);
+     
+    panel.add(0,2,1,1,dump);
+    panel.add(0,3,1,1,verbose);
+    
+    panel.add(0,4,1,1,tableG);
+    panel.add(0,5,1,1,switchG);
+    panel.add(0,6,1,1,packG);
+
+    panel.add(1,2,1,1,jlex);
+    panel.add(1,3,1,1,no_minimize);
+    panel.add(1,4,1,1,no_backup);
+    panel.add(1,5,1,1,time);
+    panel.add(1,6,1,1,dot);
+         
+    add("Center",panel);
+    
+    updateState();
+  }
+  
+  private void setGenMethod() {
+    if ( tableG.getState() ) {
+      Options.gen_method = Options.TABLE;
+      return;
+    }
+    
+    if ( switchG.getState() ) {
+      Options.gen_method = Options.SWITCH;
+      return;
+    }
+    
+    if ( packG.getState() ) {
+      Options.gen_method = Options.PACK;
+      return;
+    }
+  }
+
+  private void updateState() {
+    dump.setState(Options.dump);
+    verbose.setState(Options.verbose);
+    jlex.setState(Options.jlex);
+    no_minimize.setState(Options.no_minimize); 
+    no_backup.setState(Options.no_backup);
+    time.setState(Options.time);
+    dot.setState(Options.dot);
+
+    tableG.setState(Options.gen_method == Options.TABLE);
+    switchG.setState(Options.gen_method == Options.SWITCH);
+    packG.setState(Options.gen_method == Options.PACK);     
+  }
+
+  private void setDefaults() {
+    Options.setDefaults();
+    updateState();
+  }
+
+  public void close() {
+    hide();
   }
 
 }
