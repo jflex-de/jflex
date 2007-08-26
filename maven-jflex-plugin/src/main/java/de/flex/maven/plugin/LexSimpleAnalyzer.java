fe5278019cc0368374c11e3defd058b55a560acb
Régis Décamps <regis.decamps@gmail.com>
2007-08-26 16:31:33 +0000

    Initial import of maven-jflex-plugin. With this maven2 plugin, it is possible to generate Java code without calling the ant task in Maven. History is in http://jamwiki.svn.sourceforge.net/viewvc/jamwiki/wiki/branches/maven-jflex-plugin/?pathrev=1798
    svn-rev: trunk@187

---
+++ maven-jflex-plugin/src/main/java/de/flex/maven/plugin/LexSimpleAnalyzer.java
@@ -0,0 +1,71 @@
+package de.flex.maven.plugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import de.flex.maven.plugin.ClassInfo;
+public class LexSimpleAnalyzer {
+	public static final String DEFAULT_NAME = "Yylex";
+
+	/**
+	 * Guess the package and class name, based on this grammar definition. Does
+	 * not overide the Mojo configuration if it exist.
+	 * 
+	 * @return The name of the java code to generate.
+	 * 
+	 * Credit goes to the authors of the maven-jlex-plugin
+	 * 
+	 * @throws IOException
+	 * @author Rafal Mantiuk (Rafal.Mantiuk@bellstream.pl)
+	 * @author Gerwin Klein (lsf@jflex.de)
+	 * @throws FileNotFoundException
+	 *             if the lex file does not exist
+	 * 
+	 */
+	protected static ClassInfo guessPackageAndClass(File lexFile)
+			throws FileNotFoundException, IOException {
+		LineNumberReader reader = new LineNumberReader(new FileReader(lexFile));
+
+		ClassInfo classInfo=new ClassInfo();
+		// TODO Reuse code from parser if it exists.
+		while (classInfo.className == null || classInfo.packageName == null) {
+			String line = reader.readLine();
+			if (line == null)
+				break;
+
+			if (classInfo.packageName == null) {
+				int index = line.indexOf("package");
+				if (index >= 0) {
+					index += 7;
+
+					int end = line.indexOf(';', index);
+					if (end >= index) {
+						classInfo.packageName = line.substring(index, end);
+						classInfo.packageName = classInfo.packageName.trim();
+					}
+				}
+			}
+
+			if (classInfo.className == null) {
+				int index = line.indexOf("%class");
+				if (index >= 0) {
+					index += 6;
+
+					classInfo.className = line.substring(index);
+					classInfo.className = classInfo.className.trim();
+				}
+			}
+		}
+
+		// package name may be null, but class name not
+		if (classInfo.className == null) {
+			// log.warn("Could not guess class name from " +
+			// lexFile.getName()
+			// + ". Class name set to " + DEFAULT_NAME);
+			classInfo.className = DEFAULT_NAME;
+		}
+		return classInfo;
+	}
+}
