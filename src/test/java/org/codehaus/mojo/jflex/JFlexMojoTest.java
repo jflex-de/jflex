67c35829e55378b362e25e3b9f1c43c434ffee3e
Régis Décamps <regis.decamps@gmail.com>
2007-10-28 10:58:38 +0000

    Improve tests * use AbstractMojoTestCase * extends test scope Contributed by Benjamin Bentmann
    svn-rev: trunk/maven-jflex-plugin@203

--- src/test/java/org/codehaus/mojo/jflex/JFlexMojoTest.java
+++ src/test/java/org/codehaus/mojo/jflex/JFlexMojoTest.java
@@ -21,69 +21,113 @@ package org.codehaus.mojo.jflex;
 
 import java.io.File;
 
-import org.apache.maven.model.Model;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
-import org.codehaus.mojo.jflex.JFlexMojo;
-import org.codehaus.plexus.PlexusTestCase;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 
-public class JFlexMojoTest extends PlexusTestCase {
-	protected static final String SRC_TEST_RESOURCES_FLEX = "src/test/resources/preprocessor.jflex";
-	protected static final String EXPECTED_PACKAGE_DIR = "/org/jamwiki/parser/jflex/";
-	protected static final String EXPECTED_CLASS_NAME = "JAMWikiPreProcessor";
-	protected static final String OUTPUT_DIRECTORY = "target/test/generated";
-	
-	JFlexMojo mojo;
-	File preprocessorLex;
+/**
+ * @author Régis Décamps (decamps@users.sf.net)
+ */
+public class JFlexMojoTest extends AbstractMojoTestCase {
 
+	/**
+	 * @see org.apache.maven.plugin.testing.AbstractMojoTestCase#setUp()
+	 */
 	@Override
 	protected void setUp() throws Exception {
-		/* this is done by Maven */
-		mojo = new JFlexMojo();
-		mojo.setProject(new MavenProject(new Model()));
-		mojo.setOutputDirectory(new File(OUTPUT_DIRECTORY));
-		// parameter that I want to use frequently
-		preprocessorLex=new File(SRC_TEST_RESOURCES_FLEX);
+		// required for mojo lookups to work
+		super.setUp();
 	}
 
-	public void testInit() {
-		assertEquals(new File(OUTPUT_DIRECTORY), mojo.getOutputDirectory());
-	}
-
+	/**
+	 * @see org.codehaus.plexus.PlexusTestCase#tearDown()
+	 */
 	@Override
 	protected void tearDown() throws Exception {
-		mojo=null;
+		super.tearDown();
 	}
 	
 	/**
-	 * Test the full parser generation.
+	 * Configures an instance of the jflex mojo for the specified test case.
 	 * 
-	 * This is an integration rather than a unit-test, and is extremely useful.
+	 * @param testCase
+	 *            The name of the test case (i.e. its directory name).
+	 * @return The configured mojo.
+	 * @throws Exception
 	 */
-	public void testGenerate() throws MojoExecutionException,
-			MojoFailureException {
-		File[] lexFiles = { preprocessorLex };
-		mojo.setLexFiles(lexFiles);
+	protected JFlexMojo newMojo(String testCase) throws Exception {
+		File unitBasedir = new File(getBasedir(), "src/test/resources/unit");
+		File testPom = new File(unitBasedir, testCase + "/plugin-config.xml");
+		JFlexMojo mojo = new JFlexMojo();
+		configureMojo(mojo, "maven-jflex-plugin", testPom);
+		if (getVariableValueFromObject(mojo, "project") == null) {
+			setVariableValueToObject(mojo, "project", new MavenProjectStub());
+		}
+		return mojo;
+	}
+
+	/**
+	 * Gets the expected path to the output file from jflex.
+	 * 
+	 * @param mojo
+	 *            The jflex mojo under test.
+	 * @return The expected path to the output file from jflex.
+	 * @throws Exception
+	 */
+	protected File getExpectedOutputFile(JFlexMojo mojo) throws Exception {
+		File outDir =
+				(File) getVariableValueFromObject(mojo, "outputDirectory");
+		return new File(outDir, "/org/jamwiki/parser/jflex/"
+				+ "JAMWikiPreProcessor" + ".java");
+	}
+
+	/**
+	 * Tests configuration with a single input file.
+	 * 
+	 * @throws Exception
+	 */
+	public void testSingleFile() throws Exception {
+		JFlexMojo mojo = newMojo("single-file-test");
 		mojo.execute();
 
-		// approximative checking... Can't use md5 as the generated file
-		// contains the date.
-		File produced = new File(OUTPUT_DIRECTORY
-				+ EXPECTED_PACKAGE_DIR + EXPECTED_CLASS_NAME + ".java");
-	
-		assertTrue("produced file is a file: " + produced.getAbsolutePath(),
-				produced.isFile());
+		File produced = getExpectedOutputFile(mojo);
+		assertTrue("produced file is a file: " + produced, produced.isFile());
+
 		long size = produced.length();
 		/*
 		 * NOTE: The final file size also depends on the employed line
 		 * terminator. For this reason, the generated output will be longer on a
-		 * Windows plattform ("\r\n") than on a Unix plattform ("\n").
+		 * Windows platform ("\r\n") than on a Unix platform ("\n").
 		 */
 		boolean correctSize = (size > 26624) && (size < 28672);
 		assertTrue("size of produced file between 26k and 28k. Actual is "
 				+ size, correctSize);
-		produced.delete();
 	}
-	
+
+	/**
+	 * Tests configuration with a single input directory.
+	 * 
+	 * @throws Exception
+	 */
+	public void testSingleDir() throws Exception {
+		JFlexMojo mojo = newMojo("single-dir-test");
+		mojo.execute();
+
+		File produced = getExpectedOutputFile(mojo);
+		assertTrue("produced file is a file: " + produced, produced.isFile());
+	}
+
+	/**
+	 * Tests configuration with a single input directory containing sub
+	 * directories.
+	 * 
+	 * @throws Exception
+	 */
+	public void testRecursion() throws Exception {
+		JFlexMojo mojo = newMojo("recursion-test");
+		mojo.execute();
+
+		File produced = getExpectedOutputFile(mojo);
+		assertTrue("produced file is a file: " + produced, produced.isFile());
+	}
+
 }
