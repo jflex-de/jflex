0f59083a5b1b6a550a926d5ecaa645e764d2fe8c
Régis Décamps <regis.decamps@gmail.com>
2009-01-30 10:52:51 +0000

    merged from /branches/oldtrunk:
    POM
    * upgrade artifactid to 1.4.2-snapshot
    * follow JFlex to version 1.4.2
    * keeps Java 1.3 compatibility
    * set repository to http://jflex.sf.net/m2repo (synced with central)
    
    src
    * keeps Java 1.3 compatibility
    
    site
    * conforms to Sourceforge logo rule
    * updated usage, since our m2repo is now in sync with central
    * added changelog
    
    
    svn-rev: trunk/maven-jflex-plugin@420

--- src/main/java/org/codehaus/mojo/jflex/JFlexMojo.java
+++ src/main/java/org/codehaus/mojo/jflex/JFlexMojo.java
@@ -32,7 +32,6 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.mojo.jflex.ClassInfo;
 
 import JFlex.Main;
 import JFlex.Options;
