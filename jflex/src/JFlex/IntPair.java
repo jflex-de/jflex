b9301cc7b79b70aab593d4c4fee1f9bfe1697a35
Gerwin Klein <lsf37@doclsf.de>
2009-01-31 12:42:12 +0000

    1.4.3 release tag
    svn-rev: tags/release_1_4_3@434

--- jflex/src/JFlex/IntPair.java
+++ jflex/src/JFlex/IntPair.java
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * JFlex 1.4.2                                                             *
- * Copyright (C) 1998-2008  Gerwin Klein <lsf@jflex.de>                    *
+ * JFlex 1.4.3                                                             *
+ * Copyright (C) 1998-2009  Gerwin Klein <lsf@jflex.de>                    *
  * All rights reserved.                                                    *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
@@ -27,7 +27,7 @@ package JFlex;
  * Used in NFA to represent a partial NFA by its start and end state.
  *
  * @author Gerwin Klein
- * @version JFlex 1.4.2, $Revision$, $Date$
+ * @version JFlex 1.4.3, $Revision$, $Date$
  */
 final class IntPair {
 
--- jflex/src/JFlex/Timer.java
+++ jflex/src/JFlex/Timer.java
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * JFlex 1.4.2                                                             *
- * Copyright (C) 1998-2008  Gerwin Klein <lsf@jflex.de>                    *
+ * JFlex 1.4.3                                                             *
+ * Copyright (C) 1998-2009  Gerwin Klein <lsf@jflex.de>                    *
  * All rights reserved.                                                    *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify    *
@@ -25,7 +25,7 @@ package JFlex;
  * Not very exact, measures user time, not processor time.
  *
  * @author Gerwin Klein
- * @version JFlex 1.4.2, $Revision$, $Date$
+ * @version JFlex 1.4.3, $Revision$, $Date$
  */
 public class Timer {
 
