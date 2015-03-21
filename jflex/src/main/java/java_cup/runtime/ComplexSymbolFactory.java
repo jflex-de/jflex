13691c78271d08e6dfd1164b2dba806448b9d55a
Gerwin Klein <gerwin.klein@nicta.com.au>
2015-03-21 19:46:23 +1100

    cup update continued
    added everything I accidentally left out of the last commit..
    
    (see also #175)

--- jflex/src/main/java/java_cup/runtime/ComplexSymbolFactory.java
+++ jflex/src/main/java/java_cup/runtime/ComplexSymbolFactory.java
@@ -1,5 +1,8 @@
 package java_cup.runtime;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 /**
  * Default Implementation for SymbolFactory, creates
  * plain old Symbols
@@ -10,33 +13,106 @@ package java_cup.runtime;
 
 /* *************************************************
   class DefaultSymbolFactory
-
   interface for creating new symbols  
  ***************************************************/
 public class ComplexSymbolFactory implements SymbolFactory{
     public static class Location {
         private String unit="unknown";
-        private int line, column;
+        private int line, column, offset=-1;
+	/**
+	 * Location Object
+	 * stores compilation unit, line, column and offset to the file start
+	 * @param unit      compilation unit, e.g. file name
+	 * @param line      line number
+	 * @param column    column number
+	 * @param offset    offset from file start
+	 */
+    public Location(String unit, int line, int column, int offset){
+	   this(unit,line,column);
+	   this.offset=offset;
+	}
+	/**
+	 * Location Object
+	 * stores compilation unit, line and column
+	 * @param unit      compilation unit, e.g. file name
+	 * @param line      line number
+	 * @param column    column number
+	 */
         public Location(String unit, int line, int column){
             this.unit=unit;
             this.line=line;
             this.column=column;
         }
+	/**
+	 * Location Object
+	 * stores line, column and offset to the file start
+	 * @param line      line number
+	 * @param column    column number
+	 * @param offset    offset from file start
+	 */
+        public Location(int line, int column, int offset){
+	    this(line,column);
+	    this.offset=offset;
+	}
+	/**
+	 * Location Object
+	 * stores line and column 
+	 * @param line      line number
+	 * @param column    column number
+	 */
         public Location(int line, int column){
             this.line=line;
             this.column=column;
         }
-        public String toString(){
-            return unit+":"+line+"/"+column;
-        }
+	/**
+	 * getColumn
+	 * @returns column if known, else -1
+	 */
         public int getColumn(){
             return column;
         }
+	/**
+	 * getLine
+	 * @returns line if known, else -1
+	 */
         public int getLine(){
             return line;
         }
+	/**
+	 * getUnit
+	 * @returns compilation unit if known, else 'unknown'
+	 */
         public String getUnit(){
             return unit;
+        }
+	/**
+	 * getLine
+	 * @returns line if known, else -1
+	 */
+        public String toString(){
+            return getUnit()+":"+getLine()+"/"+getColumn()+"("+offset+")";
+        }
+        /**
+         * Writes the location information directly into an XML document
+         * @param writer the destination XML Document
+         * @param orientation adds details about the orientation of this location as an attribute; often used with the strings "left" or "right"
+         * @throws XMLStreamException
+         */
+        public void toXML(XMLStreamWriter writer,String orientation) throws XMLStreamException {
+        	writer.writeStartElement("location");
+        	writer.writeAttribute("compilationunit", unit);
+        	writer.writeAttribute("orientation", orientation);
+        	writer.writeAttribute("linenumber", line+"");
+        	writer.writeAttribute("columnnumber", column+"");
+        	writer.writeAttribute("offset", offset+"");
+        	writer.writeEndElement();
+        }
+	/**
+	 * getOffset
+	 * @returns offset to start if known, else -1
+	 */
+        public int getOffset(){
+            return offset;
         }
     }
     /**
@@ -44,7 +120,7 @@ public class ComplexSymbolFactory implements SymbolFactory{
      */
     public static class ComplexSymbol extends Symbol {
         protected String name;
-        protected Location xleft,xright;
+        public Location xleft,xright;
         public ComplexSymbol(String name, int id) {
             super(id);
             this.name=name;
@@ -57,6 +133,9 @@ public class ComplexSymbolFactory implements SymbolFactory{
             if (xleft==null || xright==null) return "Symbol: "+name;
             return "Symbol: "+name+" ("+xleft+" - "+xright+")";
         }
+        public String getName(){
+        	return name;
+        }
         public ComplexSymbol(String name, int id, int state) {
             super(id,state);
             this.name=name;
@@ -68,19 +147,19 @@ public class ComplexSymbolFactory implements SymbolFactory{
             if (right!=null) this.xright= ((ComplexSymbol)right).xright;
         }
         public ComplexSymbol(String name, int id, Location left, Location right) {
-            super(id);
+            super(id,left.offset,right.offset);
             this.name=name;
             this.xleft=left;
             this.xright=right;
         }
         public ComplexSymbol(String name, int id, Symbol left, Symbol right, Object value) {
-            super(id,value);
+            super(id,left.left,right.right,value);
             this.name=name;
             if (left!=null)  this.xleft = ((ComplexSymbol)left).xleft;
             if (right!=null) this.xright= ((ComplexSymbol)right).xright;
         }
         public ComplexSymbol(String name, int id, Location left, Location right, Object value) {
-            super(id,value);
+            super(id,left.offset,right.offset,value);
             this.name=name;
             this.xleft=left;
             this.xright=right;
@@ -95,9 +174,19 @@ public class ComplexSymbolFactory implements SymbolFactory{
 
 
     // Factory methods
+    /**
+     * newSymbol
+     * creates a complex symbol with Location objects for left and right boundaries;
+     * this is used for terminals with values!
+     */
     public Symbol newSymbol(String name, int id, Location left, Location right, Object value){
         return new ComplexSymbol(name,id,left,right,value);
     }
+    /**
+     * newSymbol
+     * creates a complex symbol with Location objects for left and right boundaries;
+     * this is used for terminals without values!
+     */
     public Symbol newSymbol(String name, int id, Location left, Location right){
         return new ComplexSymbol(name,id,left,right);
     }
