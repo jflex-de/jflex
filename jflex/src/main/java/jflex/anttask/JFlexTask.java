a82a6ab2222e1f2a80959ac6abb7a55f634a79a4
Régis Décamps <decamps@users.sf.net>
2015-04-05 20:25:34 +0200

    Close LineNumber Reader
    Close LineNumber Reader to avoid an unimportant resource leak.
    
    Also reindent with 2 spaces.

--- jflex/src/main/java/jflex/anttask/JFlexTask.java
+++ jflex/src/main/java/jflex/anttask/JFlexTask.java
@@ -22,25 +22,25 @@ import java.util.regex.Pattern;
 
 /**
  * JFlex task class
- *
+ * 
  * @author Rafal Mantiuk
  * @version JFlex 1.7.0-SNAPSHOT
  */
 public class JFlexTask extends Task {
-    private static final Pattern PACKAGE_PATTERN = Pattern.compile("package\\s+(\\S+)\\s*;");
-    private static final Pattern CLASS_PATTERN = Pattern.compile("%class\\s+(\\S+)");
-    
-    private File inputFile;
+  private static final Pattern PACKAGE_PATTERN = Pattern.compile("package\\s+(\\S+)\\s*;");
+  private static final Pattern CLASS_PATTERN = Pattern.compile("%class\\s+(\\S+)");
 
-	// found out by looking into .flex file 
-	private String className = null;
-	private String packageName = null;
+  private File inputFile;
+
+  // found out by looking into .flex file
+  private String className = null;
+  private String packageName = null;
 
   /** for javac-like dest dir behaviour */
   private File destinationDir;
-	
-	/** the actual output directory (outputDir = destinationDir + package)) */
-	private File outputDir = null;
+
+  /** the actual output directory (outputDir = destinationDir + package)) */
+  private File outputDir = null;
 
   public JFlexTask() {
     // ant default is different from the rest of JFlex
@@ -50,20 +50,20 @@ public class JFlexTask extends Task {
   }
 
   public void execute() throws BuildException {
-   	try {
-      if (inputFile == null) 
+    try {
+      if (inputFile == null)
         throw new BuildException("Input file needed. Use <jflex file=\"your_scanner.flex\"/>");
 
-			if (!inputFile.canRead()) 
-				throw new BuildException("Cannot read input file "+inputFile);
+      if (!inputFile.canRead())
+        throw new BuildException("Cannot read input file " + inputFile);
 
-			try {
-      	findPackageAndClass();        
+      try {
+        findPackageAndClass();
         normalizeOutdir();
         File destFile = new File(outputDir, className + ".java");
-        
-        if (inputFile.lastModified() > destFile.lastModified()) {      
-          Main.generate(inputFile);      
+
+        if (inputFile.lastModified() > destFile.lastModified()) {
+          Main.generate(inputFile);
           if (!Options.verbose)
             System.out.println("Generated: " + destFile.getName());
         }
@@ -75,97 +75,105 @@ public class JFlexTask extends Task {
     }
   }
 
-	/**
-	 * Peek into .flex file to get package and class name
-	 * 
-	 * @throws IOException  if there is a problem reading the .flex file 
-	 */
-	public void findPackageAndClass() throws IOException {
-		// find name of the package and class in jflex source file
-		packageName = null;
-		className = null;
+  /**
+   * Peek into .flex file to get package and class name
+   * 
+   * @throws IOException
+   *           if there is a problem reading the .flex file
+   */
+  public void findPackageAndClass() throws IOException {
+    // find name of the package and class in jflex source file
+    packageName = null;
+    className = null;
 
-		LineNumberReader reader = new LineNumberReader(new FileReader(inputFile));
+    LineNumberReader reader = new LineNumberReader(new FileReader(inputFile));
+    try {
+      while (className == null || packageName == null) {
+        String line = reader.readLine();
+        if (line == null)
+          break;
 
-		while (className == null || packageName == null) {
-			String line = reader.readLine();
-			if (line == null)	break;
+        if (packageName == null) {
+          Matcher matcher = PACKAGE_PATTERN.matcher(line);
+          if (matcher.find()) {
+            packageName = matcher.group(1);
+          }
+        }
 
-			if (packageName == null) {
-                Matcher matcher = PACKAGE_PATTERN.matcher(line);
-                if (matcher.find()) {
-                    packageName = matcher.group(1);
-				}
-			}
+        if (className == null) {
+          Matcher matcher = CLASS_PATTERN.matcher(line);
+          if (matcher.find()) {
+            className = matcher.group(1);
+          }
+        }
+      }
 
-			if (className == null) {
-                Matcher matcher = CLASS_PATTERN.matcher(line);
-                if (matcher.find()) {
-                    className = matcher.group(1);
-                }
-			}
-		}
+      // package name may be null, but class name not
+      if (className == null) {
+        className = "Yylex";
+      }
+    } finally {
+      reader.close();
+    }
+  }
 
-		// package name may be null, but class name not
-		if (className == null) className = "Yylex";
-	}
-
-	/**
-	 * Sets the actual output directory if not already set. 	
-	 *
-	 * Uses javac logic to determine output dir = dest dir + package name
-	 * If not destdir has been set, output dir = parent of input file
-	 * 
-	 * Assumes that package name is already set. 
-	 */
+  /**
+   * Sets the actual output directory if not already set.
+   * 
+   * Uses javac logic to determine output dir = dest dir + package name If not
+   * destdir has been set, output dir = parent of input file
+   * 
+   * Assumes that package name is already set.
+   */
   public void normalizeOutdir() {
-  	if (outputDir != null) return;
-  	
-    // find out what the destination directory is. Append packageName to dest dir.      
+    if (outputDir != null)
+      return;
+
+    // find out what the destination directory is. Append packageName to dest
+    // dir.
     File destDir;
-    
-    // this is not the default the jflex logic, but javac-like 
+
+    // this is not the default the jflex logic, but javac-like
     if (destinationDir != null) {
       if (packageName == null) {
-    		destDir = destinationDir;
-      }
-      else {
+        destDir = destinationDir;
+      } else {
         String path = packageName.replace('.', File.separatorChar);
-        destDir = new File(destinationDir,path);
+        destDir = new File(destinationDir, path);
       }
-    } else { //save parser to the same dir as .flex
+    } else { // save parser to the same dir as .flex
       destDir = new File(inputFile.getParent());
     }
-    
+
     setOutdir(destDir);
   }
 
-	/**
-	 * @return package name of input file
-	 * 
-	 * @see #findPackageAndClass()
-	 */
-	public String getPackage() {
-		return packageName;
-	}
+  /**
+   * @return package name of input file
+   * 
+   * @see #findPackageAndClass()
+   */
+  public String getPackage() {
+    return packageName;
+  }
 
-	/**
-	 * @return class name of input file
-	 * 
-	 * @see #findPackageAndClass()
-	 */
-	public String getClassName() {
-		return className;
-	}
+  /**
+   * @return class name of input file
+   * 
+   * @see #findPackageAndClass()
+   */
+  public String getClassName() {
+    return className;
+  }
 
   public void setDestdir(File destinationDir) {
     this.destinationDir = destinationDir;
   }
 
-	public void setOutdir(File outDir) {
-		this.outputDir = outDir;
+  public void setOutdir(File outDir) {
+    this.outputDir = outDir;
     Options.setDir(outputDir);
-	}
+  }
 
   public void setFile(File file) {
     this.inputFile = file;
@@ -178,7 +186,7 @@ public class JFlexTask extends Task {
   public void setTimeStatistics(boolean displayTime) {
     Options.time = displayTime;
   }
-  
+
   public void setTime(boolean displayTime) {
     setTimeStatistics(displayTime);
   }
@@ -187,7 +195,7 @@ public class JFlexTask extends Task {
     Options.verbose = verbose;
     Options.unused_warning = verbose;
   }
-  
+
   public void setUnusedWarning(boolean warn) {
     Options.unused_warning = warn;
   }
@@ -195,7 +203,7 @@ public class JFlexTask extends Task {
   public void setSkeleton(File skeleton) {
     Options.setSkeleton(skeleton);
   }
- 
+
   public void setSkel(File skeleton) {
     setSkeleton(skeleton);
   }
@@ -203,9 +211,9 @@ public class JFlexTask extends Task {
   public void setSkipMinimization(boolean skipMin) {
     setNomin(skipMin);
   }
-  
+
   public void setNomin(boolean b) {
-  	Options.no_minimize = b;
+    Options.no_minimize = b;
   }
 
   public void setNobak(boolean b) {
@@ -223,17 +231,17 @@ public class JFlexTask extends Task {
   public void setDump(boolean b) {
     Options.dump = b;
   }
-  
-  public void setJLex(boolean b) {    
+
+  public void setJLex(boolean b) {
     Options.jlex = b;
   }
 
   public void setLegacyDot(boolean b) {
     Options.legacy_dot = b;
   }
-    
+
   // TODO: In the JFlex version after 1.6, this option will cease to exist
   public void setInputStreamCtor(boolean b) {
-    Options.emitInputStreamCtor = b; 
+    Options.emitInputStreamCtor = b;
   }
 }
