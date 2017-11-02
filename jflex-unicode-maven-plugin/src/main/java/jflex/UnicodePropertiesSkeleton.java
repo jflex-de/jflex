76c8d99dae9b7675a0170277b08a788760e9cb36
Régis Décamps <regisd@google.com>
2017-11-02 18:17:24 +0100

    Format existing code (at master/HEAD).

--- jflex-unicode-maven-plugin/src/main/java/jflex/UnicodePropertiesSkeleton.java
+++ jflex-unicode-maven-plugin/src/main/java/jflex/UnicodePropertiesSkeleton.java
@@ -14,27 +14,24 @@ import java.net.URL;
 import java.util.List;
 import java.util.ArrayList;
 
-
 /**
  * This class stores the skeleton of generated UnicodeProperties.java
  *
- * The skeleton consists of several parts that can be emitted to
- * a file. Usually there is a portion of generated code between every
- * two parts of skeleton code.
+ * <p>The skeleton consists of several parts that can be emitted to a file. Usually there is a
+ * portion of generated code between every two parts of skeleton code.
  *
- * There is a static part (the skeleton code) and state based iterator
- * part to this class. The iterator part is used to emit consecutive skeleton
- * sections to some <code>StringBuilder</code>.
+ * <p>There is a static part (the skeleton code) and state based iterator part to this class. The
+ * iterator part is used to emit consecutive skeleton sections to some <code>StringBuilder</code>.
  *
- * Copied from the version used for scanner generation.
+ * <p>Copied from the version used for scanner generation.
  */
 public class UnicodePropertiesSkeleton {
 
   /** expected number of sections in the skeleton file */
-  static final private int size = 5;
+  private static final int size = 5;
 
   /** platform specific newline */
-  static final private String NL = System.getProperty("line.separator");
+  private static final String NL = System.getProperty("line.separator");
 
   /** The skeleton */
   private List<String> sections = new ArrayList<String>();
@@ -42,18 +39,15 @@ public class UnicodePropertiesSkeleton {
   /** The next element of the skeleton to be emitted */
   private int pos = 0;
 
-
   /**
    * Creates a new skeleton (iterator) instance.
    *
    * @param skeletonFilename The name of the skeleton file to use
-   * @throws Exception If there is a problem locating or reading in the skeleton
-   *  file
+   * @throws Exception If there is a problem locating or reading in the skeleton file
    */
   public UnicodePropertiesSkeleton(String skeletonFilename) throws Exception {
     read(skeletonFilename);
   }
-
 
   /**
    * Emits the next part of the skeleton
@@ -64,11 +58,10 @@ public class UnicodePropertiesSkeleton {
     out.append(sections.get(pos++));
   }
 
-
   /**
    * Reads an external skeleton file from a BufferedReader.
    *
-   * @param  skeletonFilename The name of the skeleton file
+   * @param skeletonFilename The name of the skeleton file
    * @throws Exception if an IO error occurs
    */
   public void read(String skeletonFilename) throws Exception {
@@ -77,13 +70,12 @@ public class UnicodePropertiesSkeleton {
     URL url = loader.getResource(skeletonFilename);
 
     if (null == url) {
-      throw new Exception("Cannot locate '" + skeletonFilename
-                          + "' - aborting.");
+      throw new Exception("Cannot locate '" + skeletonFilename + "' - aborting.");
     }
     String line;
     StringBuilder section = new StringBuilder();
-    try (BufferedReader reader = new BufferedReader
-      (new InputStreamReader(url.openStream(), "UTF-8"))) {
+    try (BufferedReader reader =
+        new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
       while (null != (line = reader.readLine())) {
         if (line.startsWith("---")) {
           sections.add(section.toString());
@@ -97,9 +89,14 @@ public class UnicodePropertiesSkeleton {
         sections.add(section.toString());
       }
       if (sections.size() != size) {
-        throw new Exception("Skeleton file '" + skeletonFilename + "' has "
-                            + sections.size() + " static sections, but " + size
-                            + " were expected.");
+        throw new Exception(
+            "Skeleton file '"
+                + skeletonFilename
+                + "' has "
+                + sections.size()
+                + " static sections, but "
+                + size
+                + " were expected.");
       }
     }
   }
