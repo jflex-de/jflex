855ef63369a1ceeefe119db6146c34bbf71d9690
Gerwin Klein <gerwin.klein@data61.csiro.au>
2017-11-04 14:50:09 +1100

    automatically reformatted

--- testsuite/testcases/src/test/java/OutputAllUnicodeCharacters.java
+++ testsuite/testcases/src/test/java/OutputAllUnicodeCharacters.java
@@ -1,14 +1,14 @@
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets; 
+import java.nio.charset.StandardCharsets;
 
 public class OutputAllUnicodeCharacters {
   public static void main(String... args) throws Exception {
     String filename = "All.Unicode.characters.input";
     try (FileOutputStream stream = new FileOutputStream(filename);
-         OutputStreamWriter writer = new OutputStreamWriter(stream, StandardCharsets.UTF_8)) {
-      for (int ch = 0 ; ch <= 0x10FFFF ; ++ch) {
-        if (Character.charCount(ch) > 1 || ! Character.isSurrogate((char)ch)) {
+        OutputStreamWriter writer = new OutputStreamWriter(stream, StandardCharsets.UTF_8)) {
+      for (int ch = 0; ch <= 0x10FFFF; ++ch) {
+        if (Character.charCount(ch) > 1 || !Character.isSurrogate((char) ch)) {
           writer.write(Character.toChars(ch));
         }
       }
