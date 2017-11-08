6eb0f74768789ac77660a79d8456c6499d82abae
Régis Décamps <regisd@google.com>
2017-11-08 18:30:21 +0100

    Complete integration by asserting that the generated files exist.

--- cup-maven-plugin/src/test/java/de/jflex/plugin/cup/GenerateMojoIntegrationTest.java
+++ cup-maven-plugin/src/test/java/de/jflex/plugin/cup/GenerateMojoIntegrationTest.java
@@ -1,6 +1,7 @@
 package de.jflex.plugin.cup;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import java.io.File;
 import org.apache.maven.plugin.testing.MojoRule;
@@ -10,6 +11,7 @@ import org.junit.Test;
 
 /** Integration test for {@link GenerateMojo} on {@code /jflex/cup/sample-project/pom.xml}. */
 public class GenerateMojoIntegrationTest {
+
   private MavenProject mavenTestProject;
 
   @Rule
@@ -27,5 +29,27 @@ public class GenerateMojoIntegrationTest {
   @Test
   public void testExecution() throws Exception {
     mojoRule.executeMojo(mavenTestProject, "generate");
+    File outputDir = new File(mavenTestProject.getBasedir(), "/target/generated-sources/cup");
+    assertWithMessage("The target `cup` directory exists in the target directory")
+        .that(outputDir.exists())
+        .isTrue();
+    File generatedJavaPath = new File(outputDir, "/de/jflex/testing");
+    assertWithMessage(
+            "The target `cup` directory contains the path inferred from the package"
+                + " declaration in the cup file")
+        .that(generatedJavaPath.exists())
+        .isTrue();
+    File generatedParseFile = new File(generatedJavaPath, "MyParser.java");
+    assertWithMessage(
+            "The java path in the target `cup` directory contains the generated parser, "
+                + "using the name defined in the sample-project pom.xml")
+        .that(generatedParseFile.exists())
+        .isTrue();
+    File generatedSymbolsFile = new File(generatedJavaPath, "MySymbol.java");
+    assertWithMessage(
+            "The java path in the target `cup` directory contains the generated sym, "
+                + "using the name defined in the sample-project pom.xml")
+        .that(generatedParseFile.exists())
+        .isTrue();
   }
 }
