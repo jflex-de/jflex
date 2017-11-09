82505fec34fcb7c4acf3003fbdb65c1da96bacf1
Régis Décamps <regisd@google.com>
2017-11-09 21:01:49 +0100

    Also use BASEDIR in ant script

--- scripts/ant-build.sh
+++ scripts/ant-build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Build with ant
 
-# Multi-platform hack for:
-#SCRIPT_PATH=$(dirname "$(readlink -f $0)")
 CWD="$PWD"
-SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+BASEDIR="$(cd "$(dirname "$0")" && pwd -P)"/..
 # Provides the logi function
-source "$SCRIPT_PATH"/logger.sh
+source "$BASEDIR"/scripts/logger.sh
 # fail on error
 set -e
 
 logi "Compile with ant"
-cd "$SCRIPT_PATH"/jflex; ant gettools build test; cd ..
+cd "$BASEDIR"/jflex; ant gettools build test; cd ..
 
 cd "$CWD"
