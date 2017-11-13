6c1882164b46d886005a9ec851b0857ce18030a1
Régis Décamps <regisd@google.com>
2017-11-13 23:23:50 +0100

    Parallelize the Travis tests (#254)
    Split the Travis tests in three individual tests
    
    - **ant**  which compiles with ant only, and is very fast to return a build failure if there is one
    - **units** which compiles with Maven, builds javadoc, and runs unit tests, and executes samples
    - **regression** which compiles with Maven (without unit tests) and runs regression tests

---
+++ scripts/test-unit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Run unit tests
+
+CWD="$PWD"
+BASEDIR="$(cd "$(dirname "$0")" && pwd -P)"/..
+# Provides the logi function
+source "$BASEDIR"/scripts/logger.sh
+# Maven executable
+MVN="$BASEDIR"/mvnw
+# fail on error
+set -e
+
+logi "Compile, test and install all"
+# NB: Installs jflex in local repo
+# implies: validate, compile, test, package, verify, install
+if [[ $TRAVIS ]]; then
+  # Quiet mode shows errors only.
+  "$MVN" install --quiet
+else
+  "$MVN" install
+fi
+
+logi "Run jflex examples"
+# Some tests invoke /bin/jflex which expects the jar in /lib
+cp "$BASEDIR"/jflex/target/jflex-*.jar "$BASEDIR"/jflex/lib
+set -x
+# Each line must end with the test command to make the script exit
+# in case of error (see #242)
+cd "$BASEDIR"/jflex/examples
+cd simple-maven; mvn test; cd ..
+# Note that mvn is a likely to be a different and older version of Maven.
+cd standalone-maven; mvn test; cd ..
+# don't assume byacc/j is installed, just run lexer
+cd byaccj; make clean; make Yylex.java; cd ..
+cd cup; make clean; make; cd ..
+cd interpreter; make clean; make; cd ..
+cd java; make clean; make; cd ..
+cd zero-reader; make clean; make; cd ..
+set +x
+
+cd "$CWD"
