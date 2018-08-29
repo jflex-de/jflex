21cf71695f576a2fee8101da5cb4917582af2f42
Régis Décamps <regisd@google.com>
2018-08-29 15:13:33 +0200

    Use mvnw for all (#286)
    * Use mvnw for all.
    
    Update test-unit.sh to use mvnw. The Maven wrapper is always present.

--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -30,10 +30,10 @@ set -x
 
 cd "$BASEDIR"/jflex/examples
 logi "Example: simple-maven"
-cd simple-maven; mvn test; cd ..
+cd simple-maven; $MVN test; cd ..
 logi "Example: standalone-maven"
 # Note that mvn is a likely to be a different and older version of Maven.
-cd standalone-maven; mvn test; cd ..
+cd standalone-maven; $MVN test; cd ..
 logi "Example: byacc/j"
 # don't assume byacc/j is installed, just run lexer
 cd byaccj; make clean; make Yylex.java; cd ..
