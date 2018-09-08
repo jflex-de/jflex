c2c4fe5adfd80957dbfceed1643c60e96fd0d308
Régis Décamps <regisd@google.com>
2018-09-08 12:51:40 +0200

    Format code

--- testsuite/jflex-testsuite-maven-plugin/src/main/java/jflextest/TestCase.java
+++ testsuite/jflex-testsuite-maven-plugin/src/main/java/jflextest/TestCase.java
@@ -1,7 +1,6 @@
 package jflextest;
 
 import com.google.common.collect.ImmutableList;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -18,69 +17,45 @@ import java.util.Locale;
 
 public class TestCase {
 
-  /**
-   * command line switches for jflex invocation
-   */
+  /** command line switches for jflex invocation */
   private List<String> jflexCmdln;
 
-  /**
-   * files on which to invoke jflex
-   */
+  /** files on which to invoke jflex */
   private List<String> jflexFiles;
 
-  /**
-   * command line switches for javac invocation
-   */
+  /** command line switches for javac invocation */
   private List<String> javacExtraFiles;
 
-  /**
-   * lines with expected differences in jflex output
-   */
+  /** lines with expected differences in jflex output */
   private List<Integer> jflexDiff;
 
-  /**
-   * Test input file encoding -- defaults to UTF-8
-   */
+  /** Test input file encoding -- defaults to UTF-8 */
   private String inputFileEncoding = "UTF-8";
 
-  /**
-   * Test output file encoding -- defaults to UTF-8
-   */
+  /** Test output file encoding -- defaults to UTF-8 */
   private String outputFileEncoding = "UTF-8";
 
-  /**
-   * Single common input file for all outputs
-   */
+  /** Single common input file for all outputs */
   private String commonInputFile = null;
 
-  /**
-   * Java version this test case is intended for
-   */
+  /** Java version this test case is intended for */
   private String javaVersion = System.getProperty("java.version");
 
-  /**
-   * misc.
-   */
+  /** misc. */
   private String className;
 
   private String testName;
   private String description;
 
-  /**
-   * base directory of this test case
-   */
+  /** base directory of this test case */
   private File testPath;
 
   private boolean expectJavacFail, expectJFlexFail;
 
-  /**
-   * inputOutputFiles to invoke test.main on and compare
-   */
+  /** inputOutputFiles to invoke test.main on and compare */
   private List<InputOutput> inputOutput;
 
-  /**
-   * get- set- methods
-   */
+  /** get- set- methods */
   void setTestName(String s) {
     testName = s;
     className = testName.substring(0, 1).toUpperCase(Locale.ENGLISH) + testName.substring(1);
@@ -210,9 +185,7 @@ public class TestCase {
     }
   }
 
-  /**
-   * Check JFlex output conformance
-   */
+  /** Check JFlex output conformance */
   private void checkOutput(TestResult jflexResult) throws TestFailException {
     File expected = new File(testPath, testName + "-flex.output");
 
@@ -312,8 +285,8 @@ public class TestCase {
         + "JFlex Command line: "
         + jflexCmdln
         + (null != javacExtraFiles
-        ? " Javac Extra Files: " + Arrays.toString(javacExtraFiles.toArray())
-        : "")
+            ? " Javac Extra Files: " + Arrays.toString(javacExtraFiles.toArray())
+            : "")
         + "\n"
         + "Files to run Tester on "
         + inputOutput
