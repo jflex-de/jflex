111da23fb340cb637d8a4795af202d1346d682aa
Régis Décamps <regisd@google.com>
2018-09-16 19:55:35 +0200

    Let Travis update the aggregated-java-sources branch (#318)
    For #271
    
    * Let Travis the source:aggregate artefact to aggregate-java-sources branch
    - Run `mvn source:aggregate`
      This creates a jar containing all java sources accross all projects, _incl. generated-sources_
    - Checkout the **aggregated-java-sources** branch in a `repo` directory
    - git rm all
    - explode the jflex-parent-VERSION-sources.jar
    - remove unrelated sources (e.g. plugins)
    - git add all
    - git commit
    - If running in **master**, git push. This is to avoid updating the source with PR.
    * Let Travis compile branch **aggregted-java-sources** as well
    
    Fix #271

---
+++ scripts/deploy-source-code.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Push aggregated source code back to git
+# This is inspired by https://martinrotter.github.io/it-programming/2016/08/26/pushing-git-travis/
+
+CWD="$PWD"
+BASEDIR="$(cd "$(dirname "$0")" && pwd -P)"/..
+# Provides the logi function
+source "$BASEDIR"/scripts/logger.sh
+# fail on error
+set -e
+
+git_setup() {
+  git config --global user.name "Travis CI"
+  git config --global user.email "deploy@travis-ci.org"
+}
+
+git_clone() {
+  logi "Cloning https://github.com/jflex-de/jflex/tree/aggregated-java-sources"
+  git clone --depth 1 --branch aggregated-java-sources https://github.com/jflex-de/jflex.git repo
+}
+
+update_source() {
+  version=$(ls target/jflex-*-sources.jar)
+  logi "Updating sources from $version"
+  cd repo
+  git rm -r META-INF jflex java_cup UnicodeProperties.java.skeleton
+  jar -xf ../target/jflex-*-sources.jar
+  logi "Remove unrelated sources and compile"
+  ./compile.sh
+  git add --all
+  git commit -a -m "Update from $version"
+  cd ..
+}
+
+git_push() {
+  cd repo
+  logi "Git log"
+  git --no-pager log
+  git diff --name-only HEAD^1
+  logi "Push to https://github.com/jflex-de/jflex/tree/aggregated-java-sources"
+  git push
+  cd ..
+}
+
+if [ -z "$CI" ] || [ "_$TEST_SUITE" -eq "_unit" ]; then
+  git_setup
+  git_clone
+  update_source
+fi
+
+# Travis should only push from master ; not from pull requests
+if [ "_$TEST_SUITE" -eq "_unit" ] && [ "_$(git rev-parse --abbrev-ref HEAD)" -eq "_master" ]; then
+  git_push
+fi
