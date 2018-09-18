eca384e204edf28deb88f6674addd64ec7393487
Régis Décamps <regisd@google.com>
2018-09-18 17:54:57 +0200

    Fix script: source not found

--- scripts/deploy-source-code.sh
+++ scripts/deploy-source-code.sh
@@ -5,7 +5,7 @@
 CWD="$PWD"
 BASEDIR="$(cd "$(dirname "$0")" && pwd -P)"/..
 # Provides the logi function
-source "$BASEDIR"/scripts/logger.sh
+. "$BASEDIR"/scripts/logger.sh
 # fail on error
 set -e
 
