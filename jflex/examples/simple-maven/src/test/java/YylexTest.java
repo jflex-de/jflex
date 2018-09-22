bb752f11da06a76573a3654ee6ec5a38af83f8c2
Régis Décamps <regisd@google.com>
2018-09-22 18:42:51 +0200

    Use truth assertions in tests.

--- jflex/examples/simple-maven/src/test/java/YylexTest.java
+++ jflex/examples/simple-maven/src/test/java/YylexTest.java
@@ -1,3 +1,5 @@
+import static com.google.common.truth.Truth.assertWithMessage;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -52,7 +54,7 @@ public class YylexTest extends TestCase {
 		for (int lineNumber = 1;lineNumber!=-1; lineNumber++) {
 			String expectedLine = expectedContent.readLine();
 			String actualLine = actualContent.readLine();			
-			assertEquals("Line "+lineNumber, expectedLine, actualLine);
+			assertWithMessage("Line "+lineNumber).that(actualLine).isEqualTo(expectedLine);
 			if (expectedLine==null) lineNumber=-2; //EOF
 		}
 	}
