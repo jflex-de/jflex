a2c415fd77a96491947906def51d374cce9ef7b3
Régis Décamps <regisd@google.com>
2018-09-22 09:44:15 +0200

    Ant examoles in ant-run.sh

--- scripts/ant-build.sh
+++ scripts/ant-build.sh
@@ -9,6 +9,15 @@ source "$BASEDIR"/scripts/logger.sh
 set -e
 
 logi "Compile with ant"
-cd "$BASEDIR"/jflex; ant gettools build test; cd ..
+cd "$BASEDIR"/jflex
+ant gettools build test
+cd ..
 
+# There are also a few examples made with ant
+cd "$BASEDIR"/jflex/examples/
+cd cup-interpreter
+ant compile
+cd ..
+
+# Return to the user Initial directory
 cd "$CWD"
