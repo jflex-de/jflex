e3f4c68eea63236e614894b5ccdcaed5d5693d27
jannick0 <jannick0@users.noreply.github.com>
2018-09-23 22:27:03 +0200

    jflex.bat: JFLEX env vars definable on cmd shell (#366)
    Allow to define JFLEX_HOME and JFLEX_VERSION as environment variables.
    Also, plus quote path to jar file.

--- jflex/bin/jflex.bat
+++ jflex/bin/jflex.bat
@@ -1,8 +1,8 @@
 @echo off
-REM Please adjust JFLEX_HOME to suit your needs
+REM Please adjust JFLEX_HOME and JFLEX_VERSION to suit your needs
 REM (please do not add a trailing backslash)
 
-set JFLEX_HOME=C:\JFLEX
-set JFLEX_VERSION=1.7.1-SNAPSHOT
+if not defined JFLEX_HOME set JFLEX_HOME=C:\JFLEX
+if not defined JFLEX_VERSION set JFLEX_VERSION=1.7.1-SNAPSHOT
 
-java -Xmx128m -jar "%JFLEX_HOME%"\lib\jflex-full-%JFLEX_VERSION%.jar %*
+java -Xmx128m -jar "%JFLEX_HOME%\lib\jflex-full-%JFLEX_VERSION%.jar" %*
