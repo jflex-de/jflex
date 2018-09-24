d02ee88209d0b7dd556f5416f70344500c810a09
Régis Décamps <regisd@google.com>
2018-09-24 19:34:37 +0200

    Revert google-java-format

--- jflex/examples/simple-maven/src/test/java/YylexTest.java
+++ jflex/examples/simple-maven/src/test/java/YylexTest.java
@@ -1,3 +1,4 @@
+import static com.google.common.truth.Truth.assertTrue;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import java.io.BufferedReader;
@@ -19,40 +20,44 @@ import junit.framework.TestCase;
  */
 public class YylexTest extends TestCase {
 
-  private static final String OUTPUT_FILE = "target/output.actual";
+	private static final String OUTPUT_FILE = "target/output.actual";
 
-  /**
-   * Test that Yylex parser behaves like expected.
-   */
-  public void testOutput() throws IOException {
-    String[] argv = new String[1];
-    argv[0] = "src/test/resources/test.txt";
+	/**
+	 * Test that Yylex parser behaves like expected.
+	 *
+	 * @throws IOException
+	 */
+	public void testOutput() throws IOException {
+		String[] argv = new String[1];
+		argv[0] = "src/test/resources/test.txt";
 
-    // the Yylex prints status on stdout
-    File actual = new File("target/output.actual");
-    actual.delete();
-    FileOutputStream fos = new FileOutputStream(OUTPUT_FILE, true);
-    System.setOut(new PrintStream(fos));
+		// the Yylex prints status on stdout
+		File actual = new File("target/output.actual");
+		actual.delete();
+		FileOutputStream fos = new FileOutputStream(OUTPUT_FILE, true);
+		System.setOut(new PrintStream(fos));
 
-    Yylex.main(argv);
+		Yylex.main(argv);
 
-    fos.close();
+		fos.close();
 
-    // test actual is expected
-    File expected = new File("src/test/resources/output.good");
-    assertThat(expected.isFile()).isTrue;
-    assertThat(actual.isFile()).isTrue;
+		// test actual is expected
+		File expected = new File("src/test/resources/output.good");
+		assertThat(expected.isFile()).isTrue;
+		assertThat(actual.isFile()).isTrue;
 
-    BufferedReader actualContent = new BufferedReader(new FileReader(actual));
-    BufferedReader expectedContent = new BufferedReader(new FileReader(expected));
+		BufferedReader actualContent = new BufferedReader(
+				new FileReader(actual));
+		BufferedReader expectedContent = new BufferedReader(new FileReader(
+				expected));
 
-    for (int lineNumber = 1; lineNumber != -1; lineNumber++) {
-      String expectedLine = expectedContent.readLine();
-      String actualLine = actualContent.readLine();
-      assertWithMessage("Line " + lineNumber).that(actualLine).isEqualTo(expectedLine);
-      if (expectedLine == null) {
-        lineNumber = -2; //EOF
-      }
-    }
-  }
+		for (int lineNumber = 1; lineNumber != -1; lineNumber++) {
+			String expectedLine = expectedContent.readLine();
+			String actualLine = actualContent.readLine();
+			assertWithMessage("Line " + lineNumber)
+					.that(actualLine)
+					.isEqualTo(expectedLine);
+			if (expectedLine==null) lineNumber=-2; //EOF
+		}
+	}
 }
