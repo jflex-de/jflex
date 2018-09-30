1f46100938cea24697647cb56d555470a9e989ae
Régis Décamps <regisd@google.com>
2018-09-30 14:43:11 +0200

    Add TEST_SUITE=bazel to run Bazel on examples
    In an attempt to support all build systems in the examples,
    run bazel test on all examples.
    
    That's also the first step towards Bazel support #204

---
+++ scripts/bazel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Run bazel on examples
+
+CWD="$PWD"
+BASEDIR="$(cd "$(dirname "$0")" && pwd -P)"/..
+# Provides the logi function
+source "$BASEDIR"/scripts/logger.sh
+# fail on error
+set -e
+
+logi "Install Bazel"
+logi "============="
+curl -L https://github.com/bazelbuild/bazel/releases/download/0.16.1/bazel_0.16.1-linux-x86_64.deb -o tools/bazel-0.16.1.deb
+sudo apt-get install tools/bazel-0.16.1.deb
+
+logi "Run Bazel tests on examples"
+logi "==========================="
+cd jflex/examples
+bazel test /..
+
+cd "$CWD"
