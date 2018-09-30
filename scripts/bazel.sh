7d76a148050cee672d05999ff52fbf3f7ec252f0
Régis Décamps <regisd@google.com>
2018-09-30 18:32:49 +0200

    Attempt to fix apt-get

--- scripts/bazel.sh
+++ scripts/bazel.sh
@@ -10,8 +10,9 @@ set -e
 
 logi "Install Bazel"
 logi "============="
+mkdir -p tools
 curl -L https://github.com/bazelbuild/bazel/releases/download/0.16.1/bazel_0.16.1-linux-x86_64.deb -o tools/bazel-0.16.1.deb
-sudo apt-get install tools/bazel-0.16.1.deb
+sudo apt-get install ./tools/bazel-0.16.1.deb
 
 logi "Run Bazel tests on examples"
 logi "==========================="
