744e65f5e0e86ff5b3f4ac19f8ebc724068531b6
Régis Décamps <regisd@google.com>
2018-09-30 23:27:07 +0200

    Move the installation of bazel in the travis.yml

--- scripts/bazel.sh
+++ scripts/bazel.sh
@@ -8,15 +8,16 @@ source "$BASEDIR"/scripts/logger.sh
 # fail on error
 set -e
 
-logi "Install Bazel"
-logi "============="
-mkdir -p tools
-curl -L https://github.com/bazelbuild/bazel/releases/download/0.16.1/bazel_0.16.1-linux-x86_64.deb -o tools/bazel-0.16.1.deb
-sudo apt-get install ./tools/bazel-0.16.1.deb
-
-logi "Run Bazel tests on examples"
-logi "==========================="
+logi "Start Bazel"
+logi "==========="
 cd jflex/examples
+bazel start
+
+logi "Build everything"
+bazel build //...
+
+logi "Test everything"
+logi "==============="
 bazel test //..
 
 cd "$CWD"
