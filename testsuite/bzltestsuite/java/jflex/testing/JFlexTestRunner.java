ce45d96f22ccba0c217bde5f23d30b043ffb60b7
Régis Décamps <regisd@google.com>
2018-10-13 11:43:37 +0200

    Start JFlexTestRunner [WIP]

---
+++ testsuite/bzltestsuite/java/jflex/testing/JFlexTestRunner.java
@@ -0,0 +1,16 @@
+package jflex.testing;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+
+public class JFlexTestRunner extends Runner {
+
+  @Override
+  public Description getDescription() {
+    return null;
+  }
+
+  @Override
+  public void run(RunNotifier runNotifier) {}
+}
