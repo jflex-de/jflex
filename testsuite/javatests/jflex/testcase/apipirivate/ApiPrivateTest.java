e5383aba7096250144ae41943c5b67e68259ef91
Régis Décamps <regisd@google.com>
2018-10-13 12:06:34 +0200

    Add minimal (failing) ApiPrivateTest

---
+++ testsuite/javatests/jflex/testcase/apipirivate/ApiPrivateTest.java
@@ -0,0 +1,15 @@
+package jflex.testcase.apipirivate;
+
+import static org.junit.Assert.fail;
+
+import jflex.testing.JFlexTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JFlexTestRunner.class)
+public class ApiPrivateTest {
+  @Test
+  public void testFail() {
+    fail();
+  }
+}
