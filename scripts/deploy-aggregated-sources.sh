95389b96433fb7d7b8a7542f7525ab9b98d4a0b2
Régis Décamps <regisd@google.com>
2018-10-17 01:15:21 +0200

    Fix Travis
    the script deployer only accepts scripts

---
+++ scripts/deploy-aggregated-sources.sh
@@ -0,0 +1,5 @@
+echo "Push to https://github.com/jflex-de/jflex/tree/aggregated-java-sources"
+cd repo
+# SECURITY NOTICE: Be sure to send stdout & stderr to /dev/null so that the the ${GITHUB_TOKEN} is$
+git remote set-url --push origin "https://${GITHUB_TOKEN}@github.com/jflex-de/jflex.git" repo > $
+git push
