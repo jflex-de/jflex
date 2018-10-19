928b85249d9ec3dc1d6e223d918bf63f46d3b871
Régis Décamps <regisd@google.com>
2018-10-19 21:05:10 +0200

    Update shell script to start gui when there is no arg

--- jflex/bin/jflex.bat
+++ jflex/bin/jflex.bat
@@ -5,4 +5,11 @@ REM (please do not add a trailing backslash)
 if not defined JFLEX_HOME set JFLEX_HOME=C:\JFLEX
 if not defined JFLEX_VERSION set JFLEX_VERSION=1.7.1-SNAPSHOT
 
+IF "%1"=="" GOTO GUI
 java -Xmx128m -jar "%JFLEX_HOME%\lib\jflex-full-%JFLEX_VERSION%.jar" %*
+GOTO END
+
+:GUI
+java -Xmx128m -jar "%JFLEX_HOME%\lib\jflex-full-%JFLEX_VERSION%.jar" jflex.gui.MainFrame
+
+:END
