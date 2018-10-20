b09dabbf18cc6f4d794d94a543d34aff0dd892d2
Travis CI <deploy@travis-ci.org>
2018-10-20 10:37:45 +0000

    Pseudo-Merge commit 6ecfaac866ee248652e281eb78a05722afe781e5 Author: Régis Décamps <regisd@google.com> Date:   Sat Oct 20 12:34:17 2018 +0200
        Create module jflex.chars (#481)
    
        * Create java package jflex.chars
        * Create bazel package
    
    Updated from target/jflex-parent-1.7.1-SNAPSHOT-sources.jar

--- java/jflex/IntCharSet.java
+++ java/jflex/IntCharSet.java
@@ -12,6 +12,7 @@ package jflex;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import jflex.chars.Interval;
 import jflex.unicode.UnicodeProperties;
 
 /**
--- java/jflex/RegExp.java
+++ java/jflex/RegExp.java
@@ -11,6 +11,7 @@ package jflex;
 
 import java.util.ArrayList;
 import java.util.List;
+import jflex.chars.Interval;
 
 /**
  * Stores a regular expression of rules section in a JFlex-specification.
