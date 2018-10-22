a0ad886aeb88a3cc5f2e2f03d816e75a3431fa4c
Régis Décamps <regisd@google.com>
2018-10-22 09:43:41 +0200

    Use curl -P to save in dir

--- scripts/compile-aggregated-java-sources.sh
+++ scripts/compile-aggregated-java-sources.sh
@@ -4,8 +4,8 @@
 # Source: https://github.com/jflex-de/jflex/blob/master/scripts/compile-aggregated-sources.sh
 mkdir lib
 REPO=https://repo.maven.apache.org/maven2
-[ -x lib/ant-1.7.0.jar ] || curl -L $REPO/org/apache/ant/ant/1.7.0/ant-1.7.0.jar -P lib/ant-1.7.0.jar
-[ -x lib/auto-value-1.4.1.jar ] || curl -L $REPO/com/google/auto/value/auto-value/1.4.1/auto-value-1.4.1.jar -P lib/auto-value-1.4.1.jar
+[ -x lib/ant-1.7.0.jar ] || curl -L $REPO/org/apache/ant/ant/1.7.0/ant-1.7.0.jar -P lib
+[ -x lib/auto-value-1.4.1.jar ] || curl -L $REPO/com/google/auto/value/auto-value/1.4.1/auto-value-1.4.1.jar -P lib
 set -x
 javac -cp lib/ant-1.7.0.jar:lib/auto-value-1.4.1.jar $(find . -name '*.java')
 
