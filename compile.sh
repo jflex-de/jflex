ae9e582ff96979dc01fd97e0eae62768b08e4897
Travis CI <deploy@travis-ci.org>
2018-10-22 21:18:42 +0000

    Pseudo-Merge commit 7317bf875a6256882cdabc1396fe0915e8f326a4 Author: Régis Décamps <regisd@google.com> Date:   Mon Oct 22 22:25:20 2018 +0200
        Move compile-aggregate-sources.sh in master branch (#510)
    
        * Roll forward "Add compile-aggregate-sources.sh (#508)"
    
        This reverts commit 14da2ff816cad87004c0a3ee5f416dabcca2d860.
    
        * Commit the master compile-aggregate-java-sources.sh in the repo of "aggregated-java-sources"
    
        * Build the classpath automatically
    
    Updated from target/jflex-parent-1.7.1-SNAPSHOT-sources.jar

--- compile.sh
+++ compile.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+# Compiles the aggregated Java sources.
+# Meant to be execute in https://github.com/jflex-de/jflex/tree/aggregated-java-sources
+# Source: https://github.com/jflex-de/jflex/blob/master/scripts/compile-aggregated-sources.sh
 mkdir lib
 REPO=https://repo.maven.apache.org/maven2
-[ -x lib/ant.jar ] || curl -L $REPO/org/apache/ant/ant/1.7.0/ant-1.7.0.jar -o lib/ant.jar
-set -x
-javac -cp lib/ant.jar $(find . -name '*.java')
+CP=""
+mvnget() {
+  dep=$1
+  jarfile=$(basename $dep)
+  ls lib/$jarfile
+  if [ -f lib/$jarfile ]; then
+    echo "Using cached $jarfile"
+    return "$jarfile"
+  fi
+  wget $REPO/$dep -P lib
+  CP="$CP:lib/$jarfile"
+}
+# Remove leading ':'
+mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
+CP=${CP##:}
+javac -cp "$CP" $(find . -name '*.java')
