e48d24a77eb37ae2272c7fb52ad1c996112fa2a5
Régis Décamps <regisd@google.com>
2018-10-22 09:48:32 +0200

    Use wget to download rather than curl

--- scripts/compile-aggregated-java-sources.sh
+++ scripts/compile-aggregated-java-sources.sh
@@ -4,8 +4,18 @@
 # Source: https://github.com/jflex-de/jflex/blob/master/scripts/compile-aggregated-sources.sh
 mkdir lib
 REPO=https://repo.maven.apache.org/maven2
-[ -x lib/ant-1.7.0.jar ] || curl -L $REPO/org/apache/ant/ant/1.7.0/ant-1.7.0.jar -P lib
-[ -x lib/auto-value-1.4.1.jar ] || curl -L $REPO/com/google/auto/value/auto-value/1.4.1/auto-value-1.4.1.jar -P lib
+mvnget() {
+  dep=$1
+  jarfile=$(basename $dep)
+  ls lib/$jarfile
+  if [ -f lib/$jarfile ]; then
+    echo "Using cached $jarfile"
+    return "$jarfile"
+  fi
+  wget $REPO/$dep -P lib
+}
 set -x
+mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
+mvnget com/google/auto/value/auto-value/1.4.1/auto-value-1.4.1.jar
 javac -cp lib/ant-1.7.0.jar:lib/auto-value-1.4.1.jar $(find . -name '*.java')
 
