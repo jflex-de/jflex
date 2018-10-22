fed42a32f1758e88f65d0cff8795aace3352a177
Régis Décamps <regisd@google.com>
2018-10-22 21:24:03 +0200

    Build the classpath automatically

--- scripts/compile-aggregated-java-sources.sh
+++ scripts/compile-aggregated-java-sources.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-# Compiles the aggregated-java-sources
-# Meant to be exectute in https://github.com/jflex-de/jflex/tree/aggregated-java-sources
+# Compiles the aggregated Java sources.
+# Meant to be execute in https://github.com/jflex-de/jflex/tree/aggregated-java-sources
 # Source: https://github.com/jflex-de/jflex/blob/master/scripts/compile-aggregated-sources.sh
 mkdir lib
 REPO=https://repo.maven.apache.org/maven2
+CP=""
 mvnget() {
   dep=$1
   jarfile=$(basename $dep)
@@ -13,6 +14,9 @@ mvnget() {
     return "$jarfile"
   fi
   wget $REPO/$dep -P lib
+  CP="$CP:lib/$jarfile"
 }
+# Remove leading ':'
 mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
-javac -cp lib/ant-1.7.0.jar $(find . -name '*.java')
+CP=${CP##:}
+javac -cp "$CP" $(find . -name '*.java')
