2847409c500ae88b6736deb9c5b4ef9f2137cd80
Régis Décamps <regisd@google.com>
2018-10-23 13:10:13 +0200

    Only import auto-value-anntotions because the generated code is already in the aggregated java sources

--- scripts/compile-aggregated-java-sources.sh
+++ scripts/compile-aggregated-java-sources.sh
@@ -18,7 +18,7 @@ mvnget() {
 }
 
 mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
-mvnget com/google/auto/value/auto-value/1.4.1/auto-value-1.4.1.jar
+mvnget com/google/auto/value/auto-value-annotations/1.6.2/auto-value-annotations-1.6.2.jar
 
 CP=${CP##:}  # Remove leading ':'
 javac -cp "$CP" $(find . -name '*.java')
