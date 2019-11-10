aed150e19acd24a4b06c4624d0e8d7abc4803ecb
Travis CI <deploy@travis-ci.org>
2019-11-10 10:23:47 +0000

    Pseudo-Merge commit 0d789dade5d16247ba4518da7c52b17284439c34 Author: Régis Décamps <regisd@google.com> Date:   Sun Nov 10 11:21:23 2019 +0100
        Upgrade autovalue to v1.7 (#578)
    
        * Upgrade autovalue to v1.7
          The current version wants annotations.jar on SDK9+
          https://github.com/google/auto/issues/503
    
        * Autovalue annotations package has been split out in different jar.
    
        * Also update aggregated-java-src script
    
        * Also upgrade POM
    
    Updated from target/jflex-parent-1.8.0-SNAPSHOT-sources.jar

--- compile.sh
+++ compile.sh
@@ -19,7 +19,7 @@ mvnget() {
 }
 
 mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
-mvnget com/google/auto/value/auto-value-annotations/1.6.2/auto-value-annotations-1.6.2.jar
+mvnget com/google/auto/value/auto-value-annotations/1.7/auto-value-annotations-1.7.jar
 mvnget javax/annotation/javax.annotation-api/1.3.2/javax.annotation-api-1.3.2.jar
 
 CP=${CP##:}  # Remove leading ':'
