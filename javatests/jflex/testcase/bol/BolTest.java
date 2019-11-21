857cd28e6f746c8ed8efec00fe84692cf499d800
Régis Décamps <regisd@google.com>
2019-11-21 14:32:51 +0100

    Replace file-based test by unit tests.

--- javatests/jflex/testcase/bol/BolTest.java
+++ javatests/jflex/testcase/bol/BolTest.java
@@ -2,62 +2,84 @@ package jflex.testcase.bol;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
-import java.io.File;
-import java.io.FileNotFoundException;
+import com.google.common.io.CharSource;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Tests BOL and EOL operators.
- */
+/** Tests BOL and EOL operators. */
 public class BolTest {
-    @Test
-    public void test0() throws Exception{
-        BolScanner scanner = createScanner("bol-0.input");
-        assertThat(scanner.state).isEqualTo(State.INITIAL);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_BOL_AND_EOL);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_EOL);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_SIMPLY);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_BOL);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_EOL);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_BOL);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.SPACE);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.OTHER);
-        assertThat(scanner.yylex()).isEqualTo(State.OTHER);
-        assertThat(scanner.yylex()).isEqualTo(State.OTHER);
-        assertThat(scanner.yylex()).isEqualTo(State.OTHER);
-        assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
-        assertThat(scanner.yylex()).isEqualTo(State.END_OF_FILE);
-        assertThat(scanner.yylex()).isEqualTo(State.END_OF_FILE);
-        assertThat(scanner.yylex()).isEqualTo(State.END_OF_FILE);
-    }
+  BolScanner scanner;
 
-    private static BolScanner createScanner(final String inputFileName) throws FileNotFoundException {
-        File inputFile = new File(
-            "javatests/jflex/testcase/bol/" +
-            inputFileName);
-        if (!inputFile.exists()) {
-            throw new FileNotFoundException(inputFile.getAbsolutePath());
-        }
-            return new BolScanner(Files.newReader(inputFile,
-                Charsets.UTF_8));
-    }
+  @Before
+  public void testMustInitializeScanner() {
+      scanner = null;
+  }
+
+  @After
+  public void end() throws Exception {
+    assertThat(scanner.yylex()).isEqualTo(State.END_OF_FILE);
+  }
+
+  @Test
+  public void initial() throws Exception {
+    scanner = createScanner("");
+    assertThat(scanner.state).isEqualTo(State.INITIAL);
+  }
+
+  @Test
+  public void bolAndEol() throws Exception {
+    scanner = createScanner("hello\n");
+    assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_BOL_AND_EOL);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  @Test
+  public void eol() throws Exception {
+    scanner = createScanner("  hello\n");
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_EOL);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  @Test
+  public void hello() throws Exception {
+    scanner = createScanner("  hello  \n");
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.HELLO_SIMPLY);
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  @Test
+  public void bol() throws Exception {
+    scanner = createScanner("hello \n");
+    assertThat(scanner.yylex()).isEqualTo(State.HELLO_AT_BOL);
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  @Test
+  public void space() throws Exception {
+    scanner = createScanner(" \n");
+    assertThat(scanner.yylex()).isEqualTo(State.SPACE);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  @Test
+  public void other() throws Exception {
+    scanner = createScanner("abc\n");
+    assertThat(scanner.yylex()).isEqualTo(State.OTHER);
+    assertThat(scanner.yylex()).isEqualTo(State.OTHER);
+    assertThat(scanner.yylex()).isEqualTo(State.OTHER);
+    assertThat(scanner.yylex()).isEqualTo(State.LINE_FEED);
+  }
+
+  private static BolScanner createScanner(final String content) throws IOException {
+    return new BolScanner(CharSource.wrap(content).openStream());
+  }
 }
