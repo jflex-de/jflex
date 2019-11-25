e396186d329069b90efaecde13f4f6e687d61849
Régis Décamps <regisd@google.com>
2019-11-25 01:34:11 +0100

    Add code coverate in Cirrus CI tasks

--- scripts/send-code-coverage.sh
+++ scripts/send-code-coverage.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-mkdir -p tools
+CODACY_REPORTER_VERSION=6.1.1
+
+mkdir -p buildtools
 
 if [[ -z "${CODACY_PROJECT_TOKEN}" ]]; then
  echo "Missing \$CODACY_PROJECT_TOKEN"
  exit 1
 fi
-if [[ "${TRAVIS_PULL_REQUEST_SLUG}:${TRAVIS_BRANCH}" = ":master" ]]; then
+
+if [[ "$CIRRUS_CI" && "$CIRRUS_BRANCH" = "$CIRRUS_DEFAULT_BRANCH" ]]; then
   # https://app.codacy.com/project/regisd/jflex/dashboard
-  curl -C - -L https://github.com/codacy/codacy-coverage-reporter/releases/download/4.0.3/codacy-coverage-reporter-4.0.3-assembly.jar  -o tools/codacy-coverage-reporter.jar
+  curl -L https://github.com/codacy/codacy-coverage-reporter/releases/download/$CODACY_REPORTER_VERSION/codacy-coverage-reporter-$CODACY_REPORTER_VERSION-assembly.jar  -o buildtools/codacy-coverage-reporter-$CODACY_REPORTER_VERSION.jar
   echo "Sending code coverage report fo Codacy"
-  java -jar tools/codacy-coverage-reporter.jar report -l Java -r report-module/target/site/jacoco-aggregate/jacoco.xml
+  java -jar buildtools/codacy-coverage-reporter-$CODACY_REPORTER_VERSION.jar report -l Java -r bazel-out/_coverage/_coverage_report.dat
 else
-  echo "Only Travis merging in main branch sends code coverage reports"
+  echo "Cirrus-CI only send coverage report only for commits in master"
 fi
