9f619257d7d7321542c2718ea39354505db61c47
Régis Décamps <regisd@google.com>
2019-11-25 21:03:46 +0100

    Allow testing on branch "cirrus"

--- scripts/send-code-coverage.sh
+++ scripts/send-code-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CODACY_REPORTER_VERSION=6.1.1
-
+CODACY_JAR=buildtools/codacy-coverage-reporter-$CODACY_REPORTER_VERSION.jar
 mkdir -p buildtools
 
 if [[ -z "${CODACY_PROJECT_TOKEN}" ]]; then
@@ -8,11 +8,13 @@ if [[ -z "${CODACY_PROJECT_TOKEN}" ]]; then
  exit 1
 fi
 
-if [[ "$CIRRUS_CI" && "$CIRRUS_BRANCH" = "$CIRRUS_DEFAULT_BRANCH" ]]; then
-  # https://app.codacy.com/project/regisd/jflex/dashboard
-  curl -L https://github.com/codacy/codacy-coverage-reporter/releases/download/$CODACY_REPORTER_VERSION/codacy-coverage-reporter-$CODACY_REPORTER_VERSION-assembly.jar  -o buildtools/codacy-coverage-reporter-$CODACY_REPORTER_VERSION.jar
+if [[ "_${CIRRUS_BRANCH}" == "_${CIRRUS_DEFAULT_BRANCH}" || "_${CIRRUS_BRANCH}" == "_cirrus" ]]; then
+  if [[ ! -f "${CODACY_JAR}" ]]; then
+    curl -L https://github.com/codacy/codacy-coverage-reporter/releases/download/$CODACY_REPORTER_VERSION/codacy-coverage-reporter-$CODACY_REPORTER_VERSION-assembly.jar  -o ${CODACY_JAR}
+  fi
   echo "Sending code coverage report fo Codacy"
-  java -jar buildtools/codacy-coverage-reporter-$CODACY_REPORTER_VERSION.jar report -l Java -r bazel-out/_coverage/_coverage_report.dat
+  # https://app.codacy.com/project/regisd/jflex/dashboard
+  java -jar ${CODACY_JAR} report -l Java -r bazel-out/_coverage/_coverage_report.dat
 else
   echo "Cirrus-CI only send coverage report only for commits in master"
 fi
