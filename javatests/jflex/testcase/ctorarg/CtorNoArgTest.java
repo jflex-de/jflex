7441c3cc7a6bef47099495b1e63317975b04fea7
Régis Décamps <regisd@google.com>
2019-11-27 21:36:33 +0100

    Bazelify testcase/ctorarg-f
    Manually add a JFlex test case. I'm confused whether we expect a GenerationException, or a warning.

--- javatests/jflex/testcase/ctorarg/CtorNoArgTest.java
+++ javatests/jflex/testcase/ctorarg/CtorNoArgTest.java
@@ -1,3 +1,19 @@
 package jflex.testcase.ctorarg;
 
-public class CtorNoArgTest {}
+import jflex.core.ScannerException;
+import jflex.exceptions.GeneratorException;
+import jflex.testing.testsuite.JFlexTestRunner;
+import jflex.testing.testsuite.annotations.TestSpec;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JFlexTestRunner.class)
+@TestSpec(
+    lex = "javatests/jflex/testcase/ctorarg/ctor-no-arg.flex",
+    sysout = "javatests/jflex/testcase/ctorarg/ctor-no-arg.output",
+    generatorThrows = GeneratorException.class,
+    generatorThrowableCause = ScannerException.class)
+public class CtorNoArgTest {
+  @Test
+  public void ok() {}
+}
