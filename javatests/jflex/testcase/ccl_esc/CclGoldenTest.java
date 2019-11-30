b91b6fdc00c14b74cbffb7a12080f3dedfdc7db8
Régis Décamps <regisd@google.com>
2019-11-30 12:35:05 +0100

    Bazelify testcase/ccl-esc

---
+++ javatests/jflex/testcase/ccl_esc/CclGoldenTest.java
@@ -0,0 +1,32 @@
+// test: ccl
+
+package jflex.testcase.ccl_esc;
+
+import java.io.Reader;
+import jflex.testing.testsuite.golden.AbstractGoldenTest;
+import org.junit.Test;
+
+/**
+ * bug-test for [".."] style character escapes (#467469)
+ *
+ * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
+ * relies on golden files for testing, expecting the scanner to output logs on the {@code
+ * System.out}. Please migrate to proper unit tests, as describe in <a
+ * href="https://github.com/jflex-de/jflex/tree/master/javatests/jflex/testcase">
+ * //javatest/jflex/testcase</a>.
+ */
+// TODO Migrate this test to proper unit tests.
+public class CclGoldenTest extends AbstractGoldenTest<Ccl> {
+
+  /** Tests that the scanner was successfully generated and can be instantiated. */
+  @Test
+  public void canInstantiateScanner() throws Exception {
+    createScanner("");
+  }
+
+  /** Creates a scanner conforming to the {@code ccl.flex} specification. */
+  @Override
+  protected Ccl createScanner(Reader reader) {
+    return new Ccl(reader);
+  }
+}
