61316f2c43d1679eb4ce5cb4de33462ac901116a
Régis Décamps <regisd@google.com>
2019-11-30 23:11:04 +0100

    Regenerate the generics testcase.

--- javatests/jflex/testcase/generics/Generics4GoldenTest.java
+++ javatests/jflex/testcase/generics/Generics4GoldenTest.java
@@ -9,8 +9,7 @@ import jflex.util.scanner.ScannerFactory;
 import org.junit.Test;
 
 /**
- * Test for feature request <a href="https://github.com/jflex-de/jflex/issues/75">#75 Support
- * generics for %type and %extends</a>.
+ * Test for feature request [ 1212181 ] Support generics for %type and %extends
  *
  * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
  * relies on golden files for testing, expecting the scanner to output logs on the {@code
