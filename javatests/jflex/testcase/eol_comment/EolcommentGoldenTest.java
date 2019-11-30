3bc3929d7cb037c39bc3aeb2be6d4bb291d323e6
Régis Décamps <regisd@google.com>
2019-11-30 17:09:44 +0100

    java format

--- javatests/jflex/testcase/eol_comment/EolcommentGoldenTest.java
+++ javatests/jflex/testcase/eol_comment/EolcommentGoldenTest.java
@@ -2,23 +2,15 @@
 
 package jflex.testcase.eol_comment;
 
-import com.google.common.io.CharSource;
-import com.google.common.io.Files;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.Charset;
 import jflex.testing.testsuite.golden.AbstractGoldenTest;
-import jflex.testing.testsuite.golden.GoldenInOutFilePair;
 import jflex.util.scanner.ScannerFactory;
 import org.junit.Test;
 
 /**
  * bug #840580 [comment in jflex file]
-
-end of line comments (//) at end of action does not compile.
-(Generated closing bracket is on same line)
+ *
+ * <p>end of line comments (//) at end of action does not compile. (Generated closing bracket is on
+ * same line)
  *
  * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
  * relies on golden files for testing, expecting the scanner to output logs on the {@code
@@ -29,10 +21,8 @@ end of line comments (//) at end of action does not compile.
 // TODO Migrate this test to proper unit tests.
 public class EolcommentGoldenTest extends AbstractGoldenTest {
 
- /** Creates a scanner conforming to the {@code eolcomment.flex} specification. */
- private final ScannerFactory<Eolcomment> scannerFactory = ScannerFactory.of(Eolcomment::new);
-
-
+  /** Creates a scanner conforming to the {@code eolcomment.flex} specification. */
+  private final ScannerFactory<Eolcomment> scannerFactory = ScannerFactory.of(Eolcomment::new);
 
   /** Tests that the scanner was successfully generated and can be instantiated. */
   @Test
