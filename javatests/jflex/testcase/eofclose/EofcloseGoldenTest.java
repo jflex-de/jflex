254b4b377e4f3315abc8c3e52f4b76d9e8b3c8ee
Régis Décamps <regisd@google.com>
2019-12-01 11:06:11 +0100

    Bazelify testcase/eofclose
    cp -r /tmp/eofclose $(bazel info workspace)/javatests/jflex/testcase

---
+++ javatests/jflex/testcase/eofclose/EofcloseGoldenTest.java
@@ -0,0 +1,39 @@
+// test: eofclose
+
+package jflex.testcase.eofclose;
+
+import java.io.File;
+import jflex.testing.testsuite.golden.AbstractGoldenTest;
+import jflex.testing.testsuite.golden.GoldenInOutFilePair;
+import jflex.util.scanner.ScannerFactory;
+import org.junit.Test;
+
+/**
+ * test eofclose directive
+ *
+ * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
+ * relies on golden files for testing, expecting the scanner to output logs on the {@code
+ * System.out}. Please migrate to proper unit tests, as describe in <a
+ * href="https://github.com/jflex-de/jflex/tree/master/javatests/jflex/testcase">
+ * //javatest/jflex/testcase</a>.
+ */
+// TODO Migrate this test to proper unit tests.
+public class EofcloseGoldenTest extends AbstractGoldenTest {
+
+  /** Creates a scanner conforming to the {@code eofclose.flex} specification. */
+  private final ScannerFactory<Eofclose> scannerFactory = ScannerFactory.of(Eofclose::new);
+
+  private File testRuntimeDir = new File("javatests/jflex/testcase/eofclose");
+
+  @Test
+  public void goldenTest0() throws Exception {
+    GoldenInOutFilePair golden =
+        new GoldenInOutFilePair(
+            new File(testRuntimeDir, "eofclose-0.input"),
+            new File(testRuntimeDir, "eofclose-0.output"));
+    compareSystemOutWith(golden);
+
+    Eofclose scanner = scannerFactory.createScannerForFile(golden.inputFile);
+    scanner.yylex();
+  }
+}
