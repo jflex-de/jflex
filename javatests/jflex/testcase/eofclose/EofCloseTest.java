66ef8deefb01a2c3d888461858fb88971b1d2fb1
Régis Décamps <regisd@google.com>
2019-12-01 11:40:25 +0100

    We don't even need data files.

--- javatests/jflex/testcase/eofclose/EofCloseTest.java
+++ javatests/jflex/testcase/eofclose/EofCloseTest.java
@@ -4,11 +4,10 @@ package jflex.testcase.eofclose;
 
 import static jflex.testing.assertion.MoreAsserts.assertThrows;
 
-import com.google.common.io.Files;
+import com.google.common.io.CharSource;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 import jflex.testing.testsuite.golden.AbstractGoldenTest;
 import org.junit.Test;
 
@@ -19,8 +18,13 @@ public class EofCloseTest extends AbstractGoldenTest {
 
   @Test
   public void eofcloseDirective_closesReaderAtEof() throws Exception {
-    File inputFile = new File(testRuntimeDir, "eofclose-0.input");
-    Reader reader = Files.newReader(inputFile, StandardCharsets.UTF_8);
+    Reader reader =
+        CharSource.wrap(
+                "" + //
+                    "bla\n" + // bla
+                    "blub\n" + // blub
+                    "\n")
+            .openStream();
     Eofclose scanner = new Eofclose(reader);
     while (scanner.yylex() != Eofclose.YYEOF) {}
     assertThrows("The scanner should close the reader at EOF", IOException.class, reader::read);
