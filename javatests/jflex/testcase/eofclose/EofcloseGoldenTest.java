ace3da6742f768563f8d03557ec70fe1c0e19a80
Régis Décamps <regisd@google.com>
2019-12-01 11:20:47 +0100

    Move java code from .jflex to .java

--- javatests/jflex/testcase/eofclose/EofcloseGoldenTest.java
+++ javatests/jflex/testcase/eofclose/EofcloseGoldenTest.java
@@ -3,6 +3,10 @@
 package jflex.testcase.eofclose;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import jflex.testing.testsuite.golden.AbstractGoldenTest;
 import jflex.testing.testsuite.golden.GoldenInOutFilePair;
 import jflex.util.scanner.ScannerFactory;
@@ -35,5 +39,40 @@ public class EofcloseGoldenTest extends AbstractGoldenTest {
 
     Eofclose scanner = scannerFactory.createScannerForFile(golden.inputFile);
     scanner.yylex();
+  }
+
+  public static void main(String argv[]) {
+    int firstFilePos = 0;
+    String encodingName = "UTF-8";
+    if (argv[0].equals("--encoding")) {
+      firstFilePos = 2;
+      encodingName = argv[1];
+      try {
+        java.nio.charset.Charset.forName(encodingName); // Side-effect: is encodingName valid?
+      } catch (Exception e) {
+        System.out.println("Invalid encoding '" + encodingName + "'");
+        return;
+      }
+    }
+    for (int i = firstFilePos; i < argv.length; i++) {
+      Eofclose scanner;
+      Reader reader = null;
+      try {
+        FileInputStream stream = new FileInputStream(argv[i]);
+        reader = new InputStreamReader(stream, encodingName);
+        scanner = new Eofclose(reader);
+        scanner.yylex();
+      } catch (IOException e) {
+        System.out.println("IO error scanning file \"" + argv[i] + "\"");
+        System.out.println(e);
+        return;
+      }
+      try {
+        reader.read();
+        System.out.println("Reader still open.");
+      } catch (IOException e) {
+        System.out.println("Reader closed. Exception is: " + e);
+      }
+    }
   }
 }
