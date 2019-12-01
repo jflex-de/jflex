d44e99d357051829b1acc67d87910dbeb32428ab
Régis Décamps <regisd@google.com>
2019-12-01 11:50:00 +0100

    Use ScannerFactory

--- javatests/jflex/testcase/eofclose/EofCloseTest.java
+++ javatests/jflex/testcase/eofclose/EofCloseTest.java
@@ -5,28 +5,30 @@ package jflex.testcase.eofclose;
 import static jflex.testing.assertion.MoreAsserts.assertThrows;
 
 import com.google.common.io.CharSource;
-import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import jflex.testing.testsuite.golden.AbstractGoldenTest;
+import jflex.util.scanner.ScannerFactory;
 import org.junit.Test;
 
 /** Test for {@code eofclose} directive. */
 public class EofCloseTest extends AbstractGoldenTest {
 
-  private File testRuntimeDir = new File("javatests/jflex/testcase/eofclose");
-
   @Test
   public void eofcloseDirective_closesReaderAtEof() throws Exception {
-    Reader reader =
+    CharSource content =
         CharSource.wrap(
-                "" + //
-                    "bla\n" + // bla
-                    "blub\n" + // blub
-                    "\n")
-            .openStream();
-    Eofclose scanner = new Eofclose(reader);
+            "" + //
+                "bla\n" + // bla
+                "blub\n" + // blub
+                "\n");
+    Reader reader = content.openStream();
+    ScannerFactory<Eofclose> scannerFactory = ScannerFactory.of(Eofclose::new);
+    Eofclose scanner = scannerFactory.createForReader(reader);
     while (scanner.yylex() != Eofclose.YYEOF) {}
-    assertThrows("The scanner should close the reader at EOF", IOException.class, reader::read);
+    assertThrows(
+        "The scanner should close the reader at EOF, making reading further impossible",
+        IOException.class,
+        reader::read);
   }
 }
