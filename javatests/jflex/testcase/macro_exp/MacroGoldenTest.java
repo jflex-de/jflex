f06c0e97cb6f48c6dd41d200b719a5fcc1ab8602
Régis Décamps <regisd@google.com>
2019-12-08 17:59:32 +0100

    Bazelify testcase/macro-exp

---
+++ javatests/jflex/testcase/macro_exp/MacroGoldenTest.java
@@ -0,0 +1,31 @@
+// test: macro
+
+package jflex.testcase.macro_exp;
+
+import jflex.testing.testsuite.golden.AbstractGoldenTest;
+import jflex.util.scanner.ScannerFactory;
+import org.junit.Test;
+
+/**
+ * bug #611118 [Macro error on regexp negation (!)]
+ *
+ * <p>wrong warnings for negated macros
+ *
+ * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
+ * relies on golden files for testing, expecting the scanner to output logs on the {@code
+ * System.out}. Please migrate to proper unit tests, as describe in <a
+ * href="https://github.com/jflex-de/jflex/tree/master/javatests/jflex/testcase">
+ * //javatest/jflex/testcase</a>.
+ */
+// TODO Migrate this test to proper unit tests.
+public class MacroGoldenTest extends AbstractGoldenTest {
+
+  /** Creates a scanner conforming to the {@code macro.flex} specification. */
+  private final ScannerFactory<Macro> scannerFactory = ScannerFactory.of(Macro::new);
+
+  /** Tests that the scanner was successfully generated and can be instantiated. */
+  @Test
+  public void canInstantiateScanner() throws Exception {
+    scannerFactory.createScannerWithContent("");
+  }
+}
