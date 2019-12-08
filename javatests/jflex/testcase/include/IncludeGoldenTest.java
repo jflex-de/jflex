1409cb48d4328f2ba764d1174caab32d9310a6a0
Régis Décamps <regisd@google.com>
2019-12-08 15:09:06 +0100

    Bazelify testcase/include

---
+++ javatests/jflex/testcase/include/IncludeGoldenTest.java
@@ -0,0 +1,41 @@
+// test: include
+
+package jflex.testcase.include;
+
+import java.io.File;
+import jflex.testing.testsuite.golden.AbstractGoldenTest;
+import jflex.testing.testsuite.golden.GoldenInOutFilePair;
+import jflex.util.scanner.ScannerFactory;
+import org.junit.Test;
+
+/**
+ * tests include files, i.e. yyPushStream and yyPopStream bug #495422
+ *
+ * <p>Note: This test was generated from {@code jflex-testsuite-maven-plugin} test cases. The test
+ * relies on golden files for testing, expecting the scanner to output logs on the {@code
+ * System.out}. Please migrate to proper unit tests, as describe in <a
+ * href="https://github.com/jflex-de/jflex/tree/master/javatests/jflex/testcase">
+ * //javatest/jflex/testcase</a>.
+ */
+// TODO Migrate this test to proper unit tests.
+public class IncludeGoldenTest extends AbstractGoldenTest {
+
+  /** Creates a scanner conforming to the {@code include.flex} specification. */
+  private final ScannerFactory<Include> scannerFactory = ScannerFactory.of(Include::new);
+
+  private File testRuntimeDir = new File("javatests/jflex/testcase/include");
+
+  @Test
+  public void goldenTest0() throws Exception {
+    GoldenInOutFilePair golden =
+        new GoldenInOutFilePair(
+            new File(testRuntimeDir, "include-0.input"),
+            new File(testRuntimeDir, "include-0.output"));
+    compareSystemOutWith(golden);
+
+    Include scanner = scannerFactory.createScannerForFile(golden.inputFile);
+    while (!scanner.yyatEOF()) {
+      System.out.println(scanner.yylex());
+    }
+  }
+}
