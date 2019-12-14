c765bf5307bda56ff93299d77443d4cd42b7e847
Régis Décamps <regisd@google.com>
2019-12-14 18:58:57 +0100

    Add BinaryPropertiesFileScanner

---
+++ java/jflex/ucd_generator/scanner/AbstractBinaryPropertiesFileScanner.java
@@ -0,0 +1,7 @@
+package jflex.ucd_generator.scanner;
+
+/**
+ * Scans the common multiple binary property Unicode.org data file format, populating
+ * unicodeVersion.propertyValueIntervals.
+ */
+public class AbstractBinaryPropertiesFileScanner {}
