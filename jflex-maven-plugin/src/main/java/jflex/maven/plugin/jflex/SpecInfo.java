fb4c4835b85fb0b5c6b072b2eda096e2b2da94ca
Gerwin Klein <gerwin.klein@data61.csiro.au>
2019-12-28 10:40:35 +1030

    recactor: rename ClassInfo to SpecInfo; update javadoc

--- jflex-maven-plugin/src/main/java/jflex/maven/plugin/jflex/SpecInfo.java
+++ jflex-maven-plugin/src/main/java/jflex/maven/plugin/jflex/SpecInfo.java
@@ -16,43 +16,44 @@ import java.util.Objects;
 import java.util.Set;
 import javax.annotation.Nullable;
 
-class ClassInfo {
+class SpecInfo {
 
+  /** name of the generated class */
   final String className;
 
   /** dot-separated package name. Empty string for the default package. */
   final String packageName;
 
+  /** set of files recursively included from the lex spec */
   final Set<File> includedFiles;
 
-  ClassInfo(String className, @Nullable String packageName) {
+  SpecInfo(String className, @Nullable String packageName) {
     this(className, packageName, Collections.emptySet());
   }
 
-  ClassInfo(String className, @Nullable String packageName, Set<File> includedFiles) {
+  SpecInfo(String className, @Nullable String packageName, Set<File> includedFiles) {
     this.className = className;
     this.packageName = Strings.nullToEmpty(packageName);
     this.includedFiles = new HashSet<>(includedFiles);
   }
 
-  /** Compares the className and packageName of instances, ignoring includedFiles. */
   @Override
   public boolean equals(Object obj) {
     if (this == obj) {
       return true;
     }
-    if (!(obj instanceof ClassInfo)) {
+    if (!(obj instanceof SpecInfo)) {
       return false;
     }
-    ClassInfo other = (ClassInfo) obj;
+    SpecInfo other = (SpecInfo) obj;
     return Objects.equals(className, other.className)
-        && Objects.equals(packageName, other.packageName);
+        && Objects.equals(packageName, other.packageName)
+        && Objects.equals(includedFiles, other.includedFiles);
   }
 
-  /** Computes a hash code from the className and packageName, ignoring includedFiles. */
   @Override
   public int hashCode() {
-    return Objects.hash(className, packageName);
+    return Objects.hash(className, packageName, includedFiles);
   }
 
   /**
