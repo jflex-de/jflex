91f5845702a75dfa5f36fecaee52d1d4000835e0
Gerwin Klein <lsf@jflex.de>
2020-02-26 16:24:32 +0800

    bump version: JFlex 1.8.0-SNAPSHOT -> 1.8.0

--- jflex/src/test/java/jflex/skeleton/SkeletonTest.java
+++ jflex/src/test/java/jflex/skeleton/SkeletonTest.java
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * JFlex 1.8.0-SNAPSHOT                                                    *
+ * JFlex 1.8.0                                                             *
  * Copyright (C) 1998-2018  Gerwin Klein <lsf@jflex.de>                    *
  * All rights reserved.                                                    *
  *                                                                         *
@@ -22,7 +22,7 @@ import org.junit.Test;
  *
  * @author Gerwin Klein
  * @author Régis Décamps
- * @version JFlex 1.8.0-SNAPSHOT
+ * @version JFlex 1.8.0
  */
 public class SkeletonTest {
 
--- jflex/src/test/java/jflex/unicode/UnicodePropertiesTest.java
+++ jflex/src/test/java/jflex/unicode/UnicodePropertiesTest.java
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * JFlex 1.8.0-SNAPSHOT                                                    *
+ * JFlex 1.8.0                                                             *
  * Copyright (c) 2008  Steve Rowe                                          *
  * All rights reserved.                                                    *
  *                                                                         *
