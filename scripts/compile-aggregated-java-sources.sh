24e448fcbadbda452af443506fd004850b058f9f
Gerwin Klein <lsf@jflex.de>
2020-02-27 18:32:51 +0800

    aggregated-java-sources: cup_runtime now external dependency

--- scripts/compile-aggregated-java-sources.sh
+++ scripts/compile-aggregated-java-sources.sh
@@ -21,6 +21,8 @@ mvnget() {
 mvnget org/apache/ant/ant/1.7.0/ant-1.7.0.jar
 mvnget com/google/auto/value/auto-value-annotations/1.7/auto-value-annotations-1.7.jar
 mvnget javax/annotation/javax.annotation-api/1.3.2/javax.annotation-api-1.3.2.jar
+mvnget com/github/vbmacher/java-cup-runtime/11b-20160615/java-cup-runtime-11b-20160615.jar
+
 
 CP=${CP##:}  # Remove leading ':'
 javac -cp "$CP" $(find . -name '*.java')
