cfdf35eae607f72a90df490d9fe1e2c31f2350f3
Gerwin Klein <lsf@jflex.de>
2020-05-03 11:13:07 +0800

    bazel test case for %7bit

---
+++ javatests/jflex/testcase/sevenbit/SevenBitTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2020, Gerwin Klein <lsf@jflex.de>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package jflex.testcase.sevenbit;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import jflex.util.scanner.ScannerFactory;
+import org.junit.Test;
+
+/** Test generation of a 7-bit scanner */
+public class SevenBitTest {
+  private final ScannerFactory<SevenBit> scannerFactory = ScannerFactory.of(SevenBit::new);
+
+  @Test
+  public void run() throws java.io.IOException {
+    SevenBit scanner = scannerFactory.createScannerWithContent("abc\n\n");
+    assertThat(scanner.yylex()).isEqualTo(1);
+    assertThat(scanner.yylex()).isEqualTo(1);
+    assertThat(scanner.yylex()).isEqualTo(1);
+    assertThat(scanner.yylex()).isEqualTo(0);
+    assertThat(scanner.yylex()).isEqualTo(-1);
+  }
+}
