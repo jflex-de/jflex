90e648bcaeda45d913329a69dc7780f89c0103a2
Régis Décamps <regisd@google.com>
2020-12-21 11:38:39 +0100

    Add Java AST-diff to test Unicode_x_y.java files are unchanged.

--- javatests/jflex/ucd_generator/UcdGeneratorIntegrationTest.java
+++ javatests/jflex/ucd_generator/UcdGeneratorIntegrationTest.java
@@ -1,14 +1,26 @@
 package jflex.ucd_generator;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import jflex.testing.diff.DiffOutputStream;
+import jflex.testing.javaast.BasicJavaInterpreter;
 import org.junit.Test;
 
-/** Integration test for {@link UcdGenerator} */
+/**
+ * Integration test for {@link UcdGenerator}.
+ *
+ * <p>Checks that the generated {@code Unicode_x_y} has the same data content than the current
+ * classes, using diff on the Java AST.
+ */
 public class UcdGeneratorIntegrationTest {
 
   @Test
@@ -18,6 +30,16 @@ public class UcdGeneratorIntegrationTest {
 
     File f = new File(outputDir, "Unicode_4_1.java");
     assertThat(f.exists()).isTrue();
+
+    UnicodePropertiesData expected =
+        UnicodePropertiesData.create(
+            jflex.core.unicode.data.Unicode_4_1.propertyValues,
+            jflex.core.unicode.data.Unicode_4_1.intervals,
+            jflex.core.unicode.data.Unicode_4_1.propertyValueAliases,
+            jflex.core.unicode.data.Unicode_4_1.maximumCodePoint,
+            jflex.core.unicode.data.Unicode_4_1.caselessMatchPartitions,
+            jflex.core.unicode.data.Unicode_4_1.caselessMatchPartitionSize);
+    assertUnicodeProperties(expected, f);
   }
 
   @Test
@@ -60,5 +82,66 @@ public class UcdGeneratorIntegrationTest {
     DiffOutputStream goldenOutputStream =
         new DiffOutputStream(Files.newReader(goldenFile, StandardCharsets.UTF_8));
     Files.copy(f, goldenOutputStream);
+  }
+
+  private void assertUnicodeProperties(UnicodePropertiesData expected, File src)
+      throws IOException {
+    ImmutableMap<String, Object> generated = BasicJavaInterpreter.parseJavaClass(src);
+
+    List actualPropertyValues = (List) generated.get("propertyValues");
+    assertWithMessage("propertyValues")
+        .that(actualPropertyValues)
+        .containsAllIn(expected.propertyValues());
+
+    Object actualIntervals = generated.get("intervals");
+    assertWithMessage("intervals").that(actualIntervals).isEqualTo(expected.intervals());
+
+    Object actualPropertyValuesAliases = generated.get("propertyValueAliases");
+    assertWithMessage("propertyValueAliases")
+        .that(actualPropertyValuesAliases)
+        .isEqualTo(expected.propertyValueAliases());
+
+    assertWithMessage("maximumCodePoint")
+        .that(generated.get("maximumCodePoint"))
+        .isEqualTo(expected.maximumCodePoint());
+
+    assertWithMessage("caselessMatchPartitions")
+        .that(generated.get("caselessMatchPartitions"))
+        .isEqualTo(expected.caselessMatchPartitions());
+
+    assertWithMessage("caselessMatchPartitionSize")
+        .that(generated.get("caselessMatchPartitionSize"))
+        .isEqualTo(expected.caselessMatchPartitionSize());
+  }
+
+  @AutoValue
+  abstract static class UnicodePropertiesData {
+    abstract ImmutableList<String> propertyValues();
+
+    abstract ImmutableList<String> intervals();
+
+    abstract ImmutableList<String> propertyValueAliases();
+
+    abstract int maximumCodePoint();
+
+    abstract String caselessMatchPartitions();
+
+    abstract int caselessMatchPartitionSize();
+
+    static UnicodePropertiesData create(
+        String[] propertyValues,
+        String[] intervals,
+        String[] propertyValueAliases,
+        int maximumCodePoint,
+        String caselessMatchPartitions,
+        int caselessMatchPartitionSize) {
+      return new AutoValue_UcdGeneratorIntegrationTest_UnicodePropertiesData(
+          ImmutableList.copyOf(propertyValues),
+          ImmutableList.copyOf(intervals),
+          ImmutableList.copyOf(propertyValueAliases),
+          maximumCodePoint,
+          caselessMatchPartitions,
+          caselessMatchPartitionSize);
+    }
   }
 }
