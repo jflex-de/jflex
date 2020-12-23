0e98b1a265ce6be328ea4d80095e1f3072376785
Régis Décamps <regisd@google.com>
2020-12-23 12:28:53 +0100

    Add test on SurrogateUtils

--- javatests/de/jflex/ucd_generator/ucd/SurrogateUtilsTest.java
+++ javatests/de/jflex/ucd_generator/ucd/SurrogateUtilsTest.java
@@ -2,7 +2,9 @@ package de.jflex.ucd_generator.ucd;
 
 import static com.google.common.truth.Truth.assertThat;
 import static de.jflex.ucd_generator.ucd.SurrogateUtils.isSurrogate;
+import static de.jflex.ucd_generator.ucd.SurrogateUtils.removeSurrogates;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 /** Test for {@link SurrogateUtils}. */
@@ -23,5 +25,16 @@ public class SurrogateUtilsTest {
   public void isSurrogate_surrogate() {
     assertThat(isSurrogate("block=surrogate")).isTrue();
     assertThat(isSurrogate("block=hightprivateusesurrogates")).isTrue();
+  }
+
+  @Test
+  public void removeSurrogate_surrogates() {
+    assertThat(removeSurrogates(0xd83f,0xdffe)).isEmpty();
+  }
+
+  @Test
+  public void removeSurrogate_notSurrogates() {
+    assertThat(removeSurrogates(0x0000,0x01f5))
+        .containsAllIn(ImmutableList.of(CodepointRange.create(0x0000, 0x01f5)));
   }
 }
