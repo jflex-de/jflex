a797ed601734b36e1f0918bd2bbe249a6f01201a
Régis Décamps <regisd@google.com>
2021-01-03 23:41:47 +0100

    Loop over KNWON_VERSIONS to make all build rules (#866)
    * Loop over KNWON_VERSIONS to make all build rules
    
    This fixes forgotten unicode_5_2/BUILD.bazel
    
    * Run generate.sh script again.
    
    This adds missing UnicodeAgeTest_5_2.java

--- java/de/jflex/migration/unicodedatatest/generate.sh
+++ java/de/jflex/migration/unicodedatatest/generate.sh
@@ -3,10 +3,20 @@ WS=$(bazel info workspace)
 
 # Generate BUILD files, in javatests/de/jflex/testcase/unicode/unicode_x_y/BUILD.bazel
 bazel build //java/de/jflex/migration/unicodedatatest:generate
-cp -rf ${WS}/bazel-bin/java/de/jflex/migration/unicodedatatest/javatests ${WS}
+cp -rf "${WS}"/bazel-bin/java/de/jflex/migration/unicodedatatest/javatests "${WS}"
+for f in $(find "${WS}"/javatests/de/jflex/testcase/unicode -name BUILD.bazel); do
+  chmod u+w "$f"
+  buildifier -r "$f"
+done
 
 # Generate UnicodeAgeTest_x_y.java
 bazel build //java/de/jflex/migration/unicodedatatest/testage:generate
 cp -rf ${WS}/bazel-bin/java/de/jflex/migration/unicodedatatest/testage/javatests ${WS}
 # This flex specs are sourced from the bazel generator directly and should not be submitted.
 find ${WS}/javatests/de/jflex/testcase/unicode -name 'UnicodeAge*.flex' -exec rm -rf {} \;
+
+# Google java format
+for f in $(git diff --name-only --diff-filter=MA master | grep '.java$');	do
+  chmod u+w "$f"
+  google-java-format -r "$f"
+done
