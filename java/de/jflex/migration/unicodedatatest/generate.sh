ea8232289b8f8519129ca97516fbcb5dda42a065
Régis Décamps <regisd@google.com>
2021-01-03 22:56:17 +0100

    Loop over KNWON_VERSIONS to make all build rules
    This fixes forgotten unicode_5_2/BUILD.bazel

--- java/de/jflex/migration/unicodedatatest/generate.sh
+++ java/de/jflex/migration/unicodedatatest/generate.sh
@@ -4,6 +4,10 @@ WS=$(bazel info workspace)
 # Generate BUILD files, in javatests/de/jflex/testcase/unicode/unicode_x_y/BUILD.bazel
 bazel build //java/de/jflex/migration/unicodedatatest:generate
 cp -rf ${WS}/bazel-bin/java/de/jflex/migration/unicodedatatest/javatests ${WS}
+for f in $(find ${WS}/javatests/de/jflex/testcase/unicode -name BUILD.bazel); do
+  chmod u+w "$f"
+  buildifier -r "$f"
+done
 
 # Generate UnicodeAgeTest_x_y.java
 bazel build //java/de/jflex/migration/unicodedatatest/testage:generate
