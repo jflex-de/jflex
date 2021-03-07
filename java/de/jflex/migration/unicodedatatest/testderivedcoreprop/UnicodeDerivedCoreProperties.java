274d30544a37e807bbb681676be06e3ea6a1c999
Régis Décamps <regisd@google.com>
2021-03-07 19:19:10 +0100

    Add UnicodeDerivedCorePropertiesTest_x_y test targets

--- java/de/jflex/migration/unicodedatatest/testderivedcoreprop/UnicodeDerivedCoreProperties.java
+++ java/de/jflex/migration/unicodedatatest/testderivedcoreprop/UnicodeDerivedCoreProperties.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2008-2021 Steve Rowe <sarowe@gmail.com>
  * Copyright (C) 2021 Google, LLC.
  *
  * License: https://opensource.org/licenses/BSD-3-Clause
@@ -24,19 +25,11 @@
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package de.jflex.migration.unicodedatatest;
-
-import static de.jflex.migration.unicodedatatest.testcompat.UnicodeCompatibilityProperties.COMPATIBILITY_PROPERTIES;
-import static de.jflex.migration.unicodedatatest.testderivedcoreprop.UnicodeDerivedCoreProperties.DERIVED_CORE_PROPERTIES;
+package de.jflex.migration.unicodedatatest.testderivedcoreprop;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import de.jflex.migration.unicodedatatest.base.UnicodeVersionTemplateVars;
-import de.jflex.version.Version;
 
-public class BuildFileTemplateVars extends UnicodeVersionTemplateVars {
-  public final ImmutableList<String> compatibilityProperties = COMPATIBILITY_PROPERTIES;
-  public final ImmutableList<String> derivedCoreProperties = DERIVED_CORE_PROPERTIES;
-  public ImmutableSet<String> emojiProperties;
-  public ImmutableList<Version> ages;
+public class UnicodeDerivedCoreProperties {
+
+  public static final ImmutableList<String> DERIVED_CORE_PROPERTIES =ImmutableList.of("Alphabetic");
 }
