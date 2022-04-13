2a8bb655aa3f6ed1ec1be0d4214fb2be9e1aac4c
Jesse Coultas <jesse@jessecoultas.com>
2022-04-12 22:02:45 -0500

    OffsetGen whitespace changes

--- jflex/src/test/java/jflex/generator/OffsetGen.java
+++ jflex/src/test/java/jflex/generator/OffsetGen.java
@@ -4,45 +4,42 @@ import com.pholser.junit.quickcheck.generator.GenerationStatus;
 import com.pholser.junit.quickcheck.generator.Generator;
 import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 
-/**
- * Generator for Offset data values
- */
+/** Generator for Offset data values */
 public class OffsetGen extends Generator<Integer> {
-	public OffsetGen() {
-		super(Integer.class);
-	}
+  public OffsetGen() {
+    super(Integer.class);
+  }
 
-	@Override
-	public Integer generate(SourceOfRandomness r, GenerationStatus status) {
-		int rnd = r.nextInt(1, 100);
+  @Override
+  public Integer generate(SourceOfRandomness r, GenerationStatus status) {
+    int rnd = r.nextInt(1, 100);
 
-		// 1% change of getting number 0
-		if (rnd == 1) {
-			return 0;
-		}
+    // 1% change of getting number 0
+    if (rnd == 1) {
+      return 0;
+    }
 
-		// 1% change of getting number 1
-		if (rnd == 2) {
-			return 1;
-		}
+    // 1% change of getting number 1
+    if (rnd == 2) {
+      return 1;
+    }
 
-		// 1% change of getting Integer.MAX_VALUE
-		if (rnd == 3) {
-			return Integer.MAX_VALUE;
-		}
+    // 1% change of getting Integer.MAX_VALUE
+    if (rnd == 3) {
+      return Integer.MAX_VALUE;
+    }
 
-		// 15% chance of getting a "larger" size
-		if (rnd >= 4 && rnd <= 18) {
-			return r.nextInt(200_001, 10_000_000);
-		}
+    // 15% chance of getting a "larger" size
+    if (rnd >= 4 && rnd <= 18) {
+      return r.nextInt(200_001, 10_000_000);
+    }
 
-		// 5% chance of getting a "huge" size
-		if (rnd >= 19 && rnd <= 23) {
-			return r.nextInt(10_000_001, Integer.MAX_VALUE);
-		}
+    // 5% chance of getting a "huge" size
+    if (rnd >= 19 && rnd <= 23) {
+      return r.nextInt(10_000_001, Integer.MAX_VALUE);
+    }
 
-		// 77% - normalish size
-		return r.nextInt(100, 20_000);
-	}
-
+    // 77% - normalish size
+    return r.nextInt(100, 20_000);
+  }
 }
